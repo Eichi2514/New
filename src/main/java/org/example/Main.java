4721ff55c881fd80841b493872cd82ad70f88e19
Eichi2514 <picon1317@gmail.com>
2024-05-31 21:05:55 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hev llo! welcome!");
+        System.out.printf("Hevllo" + " welcome!");
 
         }
     }
