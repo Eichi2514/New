6ac580c37baae5065f9f7f14e8525428e2d6b864
Eichi2514 <picon1317@gmail.com>
2024-05-31 21:25:07 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String date[] = a.split("[-]");
 
-        int a = 15;
+        int b = Integer.parseInt(date[0]);
+        int c = Integer.parseInt(date[1]);
 
-        System.out.printf("%d",a);
-
-        }
+        System.out.printf("%06d%07d",b,c);
     }
+}
