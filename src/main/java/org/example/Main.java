d0ec4e2b8a240ffe6ef0353fb27f5dfa212a6f64
Eichi2514 <picon1317@gmail.com>
2024-06-02 16:00:18 +0900

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,19 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); //고정
+        Scanner sc = new Scanner(System.in);
         String data = sc.nextLine();
-        for (int a = 0; a <= 100; a++)
-            System.out.println(data +":"+ a);
+        String date[] = data.split("[.]");
+
+        int a = Integer.parseInt(date[0]);
+        int b = Integer.parseInt(date[1]);
+
+        System.out.printf("%d"+"\n", a);
+        System.out.printf("%d", b);
     }
 }
