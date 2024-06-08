a2aa52f456b5cb3530b734256d583d9c0d354f15
깃Eichi2514 <picon1317@gmail.com>
2024-06-08 17:58:41 +0900

    up1027

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String data = sc.next();
-        String[] a = data.split("[:]");
+        String[] a = data.split("[.]");
 
-        int d = Integer.parseInt(a[1]);
-        System.out.println(d);
+        int d1 = Integer.parseInt(a[0]);
+        int d2 = Integer.parseInt(a[1]);
+        int d3 = Integer.parseInt(a[2]);
+        System.out.printf("%02d-%02d-%04d",d3, d2, d1);
     }
 }
