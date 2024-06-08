2bb7c3bf5a9013522a4c3877bd11e99cbcdf0573
깃Eichi2514 <picon1317@gmail.com>
2024-06-08 18:07:29 +0900

    up1028

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String data = sc.next();
-        String[] a = data.split("[.]");
+        long data = sc.nextLong();
 
-        int d1 = Integer.parseInt(a[0]);
-        int d2 = Integer.parseInt(a[1]);
-        int d3 = Integer.parseInt(a[2]);
-        System.out.printf("%02d-%02d-%04d",d3, d2, d1);
+        System.out.printf("%d", data);
     }
 }
