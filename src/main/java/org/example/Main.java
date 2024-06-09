c636bfe9c953cf847a728d19edf5e272ddbbadb8
깃Eichi2514 <picon1317@gmail.com>
2024-06-09 15:25:38 +0900

    up1030

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double data = sc.nextDouble();
+        long data = sc.nextLong();
 
-        System.out.printf("%.11f",data);
+        System.out.printf("%d",data);
     }
 }
