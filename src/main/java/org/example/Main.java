9e6d9a75bd919a72a56ff6a049d0ef6980e4a5be
Eichi2514 <picon1317@gmail.com>
2024-06-30 17:20:05 +0900

    new commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,9 +7,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         float a = sc.nextFloat();
-        float b = sc.nextFloat();
-        float c = sc.nextFloat();
-        float d = (a+b+c)/3;
+        float d = (a+b+c/3);
 
         System.out.printf("%.0f\n",a+b+c);
         System.out.printf("%.1f\n",d);
