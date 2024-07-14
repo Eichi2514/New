082a013b53578aa2f959dbbb2a0beda60ac2e5f7
Eichi2514 <picon1317@gmail.com>
2024-07-14 22:37:27 +0900

    up120911

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,37 +1,23 @@
 package org.example;
 
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] numbers = new int[4];
-        numbers[0] = 1;
-        numbers[1] = 3;
-        numbers[2] = 4;
-        numbers[3] = 6;
-//        numbers[4] = 10;
-//        numbers[5] = 4;
-//        numbers[6] = 1;
-//        numbers[7] = 1;
-//        numbers[8] = 1;
-//        numbers[9] = 1;
-        System.out.println(Solution.solution(541654));
+        System.out.println(Solution.solution("asDDd"));
     }
 }
 
 class Solution {
-    public static int solution(int order) {
-        int answer = 0;
-        String[] game = Integer.toString(order).split("");
-
-        for (int i = 0; i < game.length; i++) {
-            if (game[i].equals("3") || game[i].equals("6") || game[i].equals("9")){
-                answer++;
-            }
+    public static String solution(String my_string) {
+        char[] chars = my_string.toLowerCase().toCharArray();
+        Arrays.sort(chars);
+        String answer = "";
+        for (int i = 0; i < chars.length; i++) {
+            answer += chars[i];
         }
-
-
         return answer;
     }
 }
