2192f4f926aacd8ce5e52ebdd9172f3f54e91f3c
Eichi2514 <picon1317@gmail.com>
2024-07-20 22:23:41 +0900

    프로그래머스 : 옹알이(1)

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,23 +6,25 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-//        int[] dots = new int[6];
-//        dots[0] = -5;
-//        dots[1] = -3;
-//        dots[2] = -1;
-//        dots[3] = 1;
-//        dots[4] = 3;
-//        dots[5] = 5;
-//        System.out.println(Solution.solution(dots));
+        String[] dots = {"aya", "yee", "u", "maa", "wyeoo"};
+        System.out.println(Solution.solution(dots));
     }
 }
 
 class Solution {
-    public int[] solution(int num, int total) {
-        int[] answer = new int[num];
-        int start = (total/num) - ((num-1)/2);
-        for (int i = 0; i < num; i++) {
-            answer[i] = start+i;
+    public static int solution(String[] babbling) {
+        int answer = 0;
+        String[] bab = {"aya", "ye", "woo", "ma"};
+        for (int i = 0; i < babbling.length; i++) {
+            for (int j = 0; j < bab.length; j++) {
+                if (babbling[i].contains(bab[j])) {
+                    babbling[i] = babbling[i].replace(bab[j], " ");
+                }
+            }
+        }
+        for (int i = 0; i < babbling.length; i++) {
+            babbling[i] = babbling[i].trim();
+            if (babbling[i].equals("")) answer++;
         }
         return answer;
     }
