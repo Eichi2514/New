9f407a4055f34d2456d1ddb3120552c226d342ef
Eichi2514 <picon1317@gmail.com>
2024-07-20 18:32:22 +0900

    프로그래머스 : 평행

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -34,23 +34,12 @@ public class Main {
 class Solution {
     public int solution(int[][] dots) {
         int answer = 0;
-        if(Math.abs(dots[0][0] - dots[1][0]) == Math.abs(dots[2][0] - dots[3][0])){
-            if(Math.abs(dots[0][1] - dots[1][1]) == Math.abs(dots[2][1] - dots[3][1]) ){
-                answer = 1;
-            }
-        }
-
-        if(Math.abs(dots[0][0] - dots[2][0]) == Math.abs(dots[1][0] - dots[3][0])){
-            if(Math.abs(dots[0][1] - dots[2][1]) == Math.abs(dots[1][1] - dots[3][1]) ){
-                answer = 1;
-            }
-        }
-
-        if(Math.abs(dots[0][0] - dots[3][0]) == Math.abs(dots[2][0] - dots[1][0])){
-            if(Math.abs(dots[0][1] - dots[3][1]) == Math.abs(dots[2][1] - dots[1][1]) ){
-                answer = 1;
-            }
-        }
+        if (Math.abs((dots[0][1] - dots[1][1]) * (dots[2][0] - dots[3][0])) == Math.abs((dots[2][1] - dots[3][1]) * (dots[0][0] - dots[1][0])))
+            return 1;
+        if (Math.abs((dots[0][1] - dots[2][1]) * (dots[1][0] - dots[3][0])) == Math.abs((dots[1][1] - dots[3][1]) * (dots[0][0] - dots[2][0])))
+            return 1;
+        if (Math.abs((dots[0][1] - dots[3][1]) * (dots[1][0] - dots[2][0])) == Math.abs((dots[1][1] - dots[2][1]) * (dots[0][0] - dots[3][0])))
+            return 1;
         return answer;
     }
 }
