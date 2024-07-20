49bfbba8db2173328bec6eda6e1cd0e1b2cc4d44
Eichi2514 <picon1317@gmail.com>
2024-07-20 21:08:55 +0900

    프로그래머스 : 연속된 수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,24 +6,23 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] dots = new int[6];
-        dots[0] = -5;
-        dots[1] = -3;
-        dots[2] = -1;
-        dots[3] = 1;
-        dots[4] = 3;
-        dots[5] = 5;
-        System.out.println(Solution.solution(dots));
+//        int[] dots = new int[6];
+//        dots[0] = -5;
+//        dots[1] = -3;
+//        dots[2] = -1;
+//        dots[3] = 1;
+//        dots[4] = 3;
+//        dots[5] = 5;
+//        System.out.println(Solution.solution(dots));
     }
 }
 
 class Solution {
-    public static int solution(int[] common) {
-        int answer = 0;
-        if (common[1] - common[0] == common[2] - common[1]) {
-            answer = common[common.length - 1] + (common[1] - common[0]);
-        } else {
-            answer = common[common.length - 1] * common[1] / common[0];
+    public int[] solution(int num, int total) {
+        int[] answer = new int[num];
+        int start = (total/num) - ((num-1)/2);
+        for (int i = 0; i < num; i++) {
+            answer[i] = start+i;
         }
         return answer;
     }
