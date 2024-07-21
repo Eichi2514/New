a8f3e8370ee315cea1bfdd82c13387b6476c05cc
Eichi2514 <picon1317@gmail.com>
2024-07-21 12:38:09 +0900

    백준 : 알람시계

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,15 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int sign = 0;
-        if (a%4 == 0 && a%100 != 0 || a%400 == 0) sign = 1;
-        System.out.println(sign);
+        int b = sc.nextInt();
+        int h = 0;
+        int time = (a * 60) + b;
+        time -= 45;
+        if (time < 0) time += (24*60);
+        while (time >= 60) {
+            h++;
+            time -= 60;
+        }
+        System.out.println(h + " " + time);
     }
 }
