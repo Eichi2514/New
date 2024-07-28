3ed2a194044c54b4d2158ce84131cd792960aac3
Eichi2514 <picon1317@gmail.com>
2024-07-28 17:42:56 +0900

    백준 : 두 번째 수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,17 @@
 package org.example;
 
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int y = sc.nextInt();
-        int m = sc.nextInt();
-        int d = sc.nextInt();
-        int a = (y+m+d)/100;
-        if ((a%2) == 0) System.out.println("대박");
-        else System.out.println("그럭저럭");
+        int[] nums = new int[3];
+        for (int i = 0; i < 3; i++) {
+            nums[i] = sc.nextInt();
+        }
+        Arrays.sort(nums);
+        System.out.println(nums[1]);
     }
 }
