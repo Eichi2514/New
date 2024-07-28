181719e31199419810098d6291701cd480077257
Eichi2514 <picon1317@gmail.com>
2024-07-28 23:12:10 +0900

    프로그래머스 : 문자열 겹쳐쓰기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,20 @@
 package org.example;
 
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int tmpN = sc.nextInt();
-        String tmp = Integer.toString(tmpN);
-        String[] tmps = tmp.split("");
-        tmp = tmps[1]+tmps[0];
-        tmpN = (Integer.parseInt(tmp)*2)%100;
-        System.out.println(tmpN);
-        if (tmpN <= 50) System.out.println("GOOD");
-        else System.out.println("OH MY GOD");
+        System.out.println(Solution.solution("He11oWor1d", "lloWorl", 2));
+    }
+}
+
+class Solution {
+    public static String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+        if (my_string.length() > overwrite_string.length()) {
+            answer = answer+my_string.substring(overwrite_string.length()+s);
+        }
+        return answer;
     }
 }
