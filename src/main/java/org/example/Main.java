b5019281329f925dd23b49922285303192498e75
Eichi2514 <picon1317@gmail.com>
2024-07-28 17:39:05 +0900

    백준 : 당신의 사주를 봐 드립니다 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,14 +6,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n1 = sc.nextInt();
-        int n2 = sc.nextInt();
-        if (n1 % 2 == 0) {
-            if (n2 % 2 == 0) System.out.println("짝수+짝수=짝수");
-            else System.out.println("짝수+홀수=홀수");
-        } else {
-            if (n2 % 2 == 0) System.out.println("홀수+짝수=홀수");
-            else System.out.println("홀수+홀수=짝수");
-        }
+        int y = sc.nextInt();
+        int m = sc.nextInt();
+        int d = sc.nextInt();
+        if (((y-m+d)%10) == 0) System.out.println("대박");
+        else System.out.println("그럭저럭");
     }
 }
