24870e01ebeb314e4f35ed0ad3871d5def738891
Eichi2514 <picon1317@gmail.com>
2024-07-28 17:38:23 +0900

    백준 : 홀수와 짝수 그리고 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,33 +1,19 @@
 package org.example;
 
 
-import java.util.Arrays;
-import java.util.List;
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(bf.readLine());
-        int[][] p = new int[100][100];
-        for (int i = 0; i < n; i++) {
-            String[] xy = bf.readLine().split(" ");
-            int x = Integer.parseInt(xy[0])-1;
-            int y = Integer.parseInt(xy[1])-1;
-            for (int j = x; j < x+10; j++) {
-                for (int k = y; k < y+10; k++) {
-                    p[j][k] = 1;
-                }
-            }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        if (n1 % 2 == 0) {
+            if (n2 % 2 == 0) System.out.println("짝수+짝수=짝수");
+            else System.out.println("짝수+홀수=홀수");
+        } else {
+            if (n2 % 2 == 0) System.out.println("홀수+짝수=홀수");
+            else System.out.println("홀수+홀수=짝수");
         }
-        int count = 0;
-        for (int i = 0; i < 100; i++) {
-            for (int j = 0; j < 100; j++) {
-                if (p[i][j] == 1) count++;
-            }
-        }
-        bw.write(count+"");
-        bw.flush();
     }
 }
