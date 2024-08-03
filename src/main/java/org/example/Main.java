54bff55bcbcf58e54d6ecd28807401ac985c0147
Eichi2514 <picon1317@gmail.com>
2024-08-03 23:35:26 +0900

    프로그래머스 : 주사위 게임 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,17 +5,18 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String[] a = {"0123456789","9876543210","9999999999999"};
-        System.out.println(Solution.solution(3));
+        String[] a = {"0123456789", "9876543210", "9999999999999"};
+        System.out.println(Solution.solution(3, 5));
     }
 }
 
 class Solution {
-    public static int[][] solution(int n) {
-        int[][] answer = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            answer[i][i] = 1;
-        }
+    public static int solution(int a, int b) {
+        int answer = 0;
+        if (a % 2 != 0 && b % 2 != 0) answer += (a * a) + (b * b);
+        else if (a % 2 != 0 || b % 2 != 0) answer += 2 * (a + b);
+        else if (a > b) answer += a - b;
+        else answer += b - a;
         return answer;
     }
 }
