bcd313d0b0606d0838c801bac58d8dd74ff7b0c9
Eichi2514 <picon1317@gmail.com>
2024-08-03 23:12:33 +0900

    프로그래머스 : 배열 만들기 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,21 @@
 package org.example;
 
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
-        int[][] a = {{2, 3}, {0, 7}, {5, 9}, {6, 10}};
-        System.out.println(Solution.solution("rermgorpsam", a));
+        String[] a = {"0123456789","9876543210","9999999999999"};
+        System.out.println(Solution.solution(a,50000,5,5));
     }
 }
 
 class Solution {
-    public static String solution(String my_string, int[][] queries) {
-        String answer = "";
-        String a = "";
-        String b = "";
-        for (int i = 0; i < queries.length; i++) {
-            a = my_string.substring(0, queries[i][0]);
-            b = my_string.substring(queries[i][1] + 1, my_string.length());
-            for (int j = queries[i][1]; j >= queries[i][0]; j--) {
-                answer += my_string.charAt(j);
-            }
-            my_string = a + answer + b;
-            answer = "";
+    public static ArrayList<Integer> solution(String[] intStrs, int k, int s, int l) {
+        ArrayList<Integer> answer = new ArrayList<Integer>();
+        for (int i = 0; i < intStrs.length; i++) {
+            if (Integer.parseInt(intStrs[i].substring(s, s + l)) > k)answer.add(Integer.parseInt(intStrs[i].substring(s, s + l)));
         }
-        return my_string;
+        return answer;
     }
 }
