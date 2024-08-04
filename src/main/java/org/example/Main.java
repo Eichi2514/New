8c168b5701134f321b1540b1a99702475be47ebd
Eichi2514 <picon1317@gmail.com>
2024-08-04 17:37:07 +0900

    프로그래머스 : 문자열이 몇 번 등장하는지 세기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,21 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 //        int[] a = {1, 2, 3, 4, 5};
-        int[][] b = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
-        System.out.println(Solution.solution(b, 2));
+//        int[][] b = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
+        System.out.println(Solution.solution("banana", "ana"));
     }
 }
 
 class Solution {
-    public static int solution(int[][] board, int k) {
+    public static int solution(String myString, String pat) {
         int answer = 0;
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (i + j <= k) answer += board[i][j];
+        String[] tmp = new String[myString.length()];
+        for (int i = 0; i < myString.length(); i++) {
+            tmp[i] = myString.substring(i);
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            if (tmp[i].startsWith(pat)) {
+                answer++;
             }
         }
         return answer;
