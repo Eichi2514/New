1ce6935e791802d89b173c431c3176a70200c009
Eichi2514 <picon1317@gmail.com>
2024-08-04 15:16:49 +0900

    프로그래머스 : 문자열 뒤집기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         String[] a = {"0123456789", "9876543210", "9999999999999"};
-        System.out.println(Solution.solution(3, 5));
+        System.out.println(Solution.solution("Progra21Sremm3",6,12));
     }
 }
 
 class Solution {
-    public static int solution(int a, int b) {
-        int answer = 0;
-        if (a % 2 != 0 && b % 2 != 0) answer += (a * a) + (b * b);
-        else if (a % 2 != 0 || b % 2 != 0) answer += 2 * (a + b);
-        else if (a > b) answer += a - b;
-        else answer += b - a;
+    public static String solution(String my_string, int s, int e) {
+        String answer = "";
+        String a = my_string.substring(0, s);
+        String b = my_string.substring(e+1, my_string.length());
+        for (int i = e; i >= s; i--) {
+            answer += my_string.charAt(i) + "";
+        }
+        answer = a + answer + b;
         return answer;
     }
 }
