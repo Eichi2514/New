15c909e2a60d2681f299471ebde2a4117bdb881d
Eichi2514 <picon1317@gmail.com>
2024-08-04 15:42:54 +0900

    프로그래머스 : 문자 개수 세기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,15 +6,17 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         String[] a = {"0123456789", "9876543210", "9999999999999"};
-        System.out.println(Solution.solution("Progra21Sremm3", 2, 3));
+        System.out.println(Solution.solution("A"));
     }
 }
 
 class Solution {
-    public static String solution(String my_string, int m, int c) {
-        String answer = "";
-        for (int i = c - 1; i < my_string.length(); i += m) {
-            answer += my_string.charAt(i);
+    public static int[] solution(String my_string) {
+        int[] answer = new int[52];
+        for (int i = 0; i < my_string.length(); i++) {
+            int index = my_string.charAt(i);
+            if (index > 115) answer[index-65]++;
+            else if (index < 120) answer[index-71]++;
         }
         return answer;
     }
