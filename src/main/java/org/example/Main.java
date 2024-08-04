88f47e05b23a4d4df6fc98c3b121362131facb49
Eichi2514 <picon1317@gmail.com>
2024-08-04 16:30:15 +0900

    프로그래머스 : 특별한 이차원 배열 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,22 +5,23 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {1, 0, 0, 1};
-        System.out.println(Solution.solution("1 + 1"));
+//        int[] a = {1, 2, 3, 4, 5};
+        int[][] b = {{1,3},{0,4}};
+        System.out.println(Solution.solution(b));
     }
 }
-
 class Solution {
-    public static int solution(String binomial) {
+    public static int solution(int[][] arr) {
         int answer = 0;
-        String[] tmp = binomial.split(" ");
-        if (tmp[1].equals("+")) {
-            answer = Integer.parseInt(tmp[0]) + Integer.parseInt(tmp[2]);
-        } else if (tmp[1].equals("-")) {
-            answer = Integer.parseInt(tmp[0]) - Integer.parseInt(tmp[2]);
-        } else if (tmp[1].equals("*")) {
-            answer = Integer.parseInt(tmp[0]) * Integer.parseInt(tmp[2]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == arr[j][i]) {
+                    answer++;
+                }
+            }
         }
-        return answer;
+        if (answer == arr.length*arr[0].length) {
+            return 1;
+        } else return 0;
     }
 }
