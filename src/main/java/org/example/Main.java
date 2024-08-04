3f207a6cd3fa677a3956e3abba0421f35360a029
Eichi2514 <picon1317@gmail.com>
2024-08-04 16:38:11 +0900

    프로그래머스 : 이차원 배열 대각선 순회하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,19 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
 //        int[] a = {1, 2, 3, 4, 5};
-        int[][] b = {{1,3},{0,4}};
-        System.out.println(Solution.solution(b));
+        int[][] b = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
+        System.out.println(Solution.solution(b, 2));
     }
 }
+
 class Solution {
-    public static int solution(int[][] arr) {
+    public static int solution(int[][] board, int k) {
         int answer = 0;
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 0; j < arr[i].length; j++) {
-                if (arr[i][j] == arr[j][i]) {
-                    answer++;
-                }
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[i].length; j++) {
+                if (i + j <= k) answer += board[i][j];
             }
         }
-        if (answer == arr.length*arr[0].length) {
-            return 1;
-        } else return 0;
+        return answer;
     }
 }
