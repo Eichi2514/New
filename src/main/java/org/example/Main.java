ef33f3290f38fce7099ef151fa4c5a1d52c29b43
Eichi2514 <picon1317@gmail.com>
2024-08-04 18:06:41 +0900

    프로그래머스 : 1로 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,26 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6};
+        int[] a = {12, 4, 15, 1, 14};
 //        int[][] b = {{0, 1, 2}, {1, 2, 3}, {2, 3, 4}, {3, 4, 5}};
         System.out.println(Solution.solution(a));
     }
 }
 
 class Solution {
-    public static int[] solution(int[] arr) {
-        int idx = 1;
-        while (idx < arr.length) {
-            idx *= 2;
-        }
-        int[] answer = new int[idx];
-        for (int i = 0; i < arr.length; i++) {
-            answer[i] = arr[i];
+    public static int solution(int[] num_list) {
+        int answer = 0;
+        for (int i = 0; i < num_list.length; i++) {
+            while (num_list[i] != 1) {
+                if ((num_list[i] % 2) == 0) {
+                    num_list[i] /= 2;
+                    answer++;
+                }
+                else {
+                    num_list[i] = (num_list[i] - 1) / 2;
+                    answer++;
+                }
+            }
         }
         return answer;
     }
