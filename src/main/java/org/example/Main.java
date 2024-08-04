4299a5fa254098e304b85abaf64e4179f8380b05
Eichi2514 <picon1317@gmail.com>
2024-08-04 15:57:52 +0900

    프로그래머스 : 글자 지우기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,18 +5,20 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String[] a = {"0123456789", "9876543210", "9999999999999"};
-        System.out.println(Solution.solution("A"));
+        int[] a = {1, 16, 6, 15, 0, 10, 11, 3};
+        System.out.println(Solution.solution("apporoograpemmemprs", a));
     }
 }
 
 class Solution {
-    public static int[] solution(String my_string) {
-        int[] answer = new int[52];
-        for (int i = 0; i < my_string.length(); i++) {
-            int index = my_string.charAt(i);
-            if (index > 115) answer[index-65]++;
-            else if (index < 120) answer[index-71]++;
+    public static String solution(String my_string, int[] indices) {
+        String answer = "";
+        String[] tmp = my_string.split("");
+        for (int i = 0; i < indices.length; i++) {
+            tmp[indices[i]] = "";
+        }
+        for (int i = 0; i < tmp.length; i++) {
+            answer += tmp[i];
         }
         return answer;
     }
