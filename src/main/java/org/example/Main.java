234647392a2ef11d3a3710ab0e841c8f8c721291
Eichi2514 <picon1317@gmail.com>
2024-08-11 13:10:08 +0900

    백준 : 세탁소 사장 동혁

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,12 +7,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] tmp = bf.readLine().split(" ");
-        int a = Integer.parseInt(tmp[0]);
-        int b = Integer.parseInt(tmp[1]);
-        String n = Integer.toString(a,b);
-        n = n.toUpperCase();
-        bw.write(n);
+        int n = Integer.parseInt(bf.readLine());
+        for (int i = 0; i < n; i++) {
+           int tmp = Integer.parseInt(bf.readLine());
+           int Q = 0;
+           while (tmp >= 25) {
+               tmp -= 25;
+               Q++;
+           }
+            bw.write(Q + " ");
+            int D = 0;
+            while (tmp >= 10) {
+                tmp -= 10;
+                D++;
+            }
+            bw.write(D + " ");
+            int N = 0;
+            while (tmp >= 5) {
+                tmp -= 5;
+                N++;
+            }
+            bw.write(N + " ");
+            int P = 0;
+            while (tmp >= 1) {
+                tmp -= 1;
+                P++;
+            }
+            bw.write(P+"");
+            bw.newLine();
+        }
         bw.flush();
     }
 }
