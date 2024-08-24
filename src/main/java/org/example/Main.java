5469e544a1ed5c9b6aca6073f8f9d04e98a66212
Eichi2514 <picon1317@gmail.com>
2024-08-24 13:11:01 +0900

    백준 : 중앙 이동 알고리즘

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,41 +1,17 @@
 package org.example;
-
-
 import java.io.*;
-
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int n = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < n; i++) {
-           int tmp = Integer.parseInt(bf.readLine());
-           int Q = 0;
-           while (tmp >= 25) {
-               tmp -= 25;
-               Q++;
-           }
-            bw.write(Q + " ");
-            int D = 0;
-            while (tmp >= 10) {
-                tmp -= 10;
-                D++;
-            }
-            bw.write(D + " ");
-            int N = 0;
-            while (tmp >= 5) {
-                tmp -= 5;
-                N++;
-            }
-            bw.write(N + " ");
-            int P = 0;
-            while (tmp >= 1) {
-                tmp -= 1;
-                P++;
-            }
-            bw.write(P+"");
-            bw.newLine();
+        int num1 = Integer.parseInt(bf.readLine());
+        int num2 = 2;
+        int sum = 0;
+        for (int i = 1; i <= num1; i++) {
+            sum = ((num2*2)-1)*((num2*2)-1);
+            num2 = (num2*2)-1;
         }
+        bw.write(sum+"");
         bw.flush();
     }
 }
