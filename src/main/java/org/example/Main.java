3dfa09bad68e4fdf3cba0f04e22257db2ebe3f6c
Eichi2514 <picon1317@gmail.com>
2024-09-29 22:37:38 +0900

    백준 : 세 막대

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,17 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while (true) {
-            String[] tmp = bf.readLine().split(" ");
-            if (tmp[0].equals("0") && tmp[1].equals("0") && tmp[2].equals("0")) break;
-            int a = Integer.parseInt(tmp[0]);
-            int b = Integer.parseInt(tmp[1]);
-            int c = Integer.parseInt(tmp[2]);
-            if (a >= b + c || b >= a + c || c >= a + b) bw.write("Invalid\n");
-            else if (a == b && b == c && a == c) bw.write("Equilateral\n");
-            else if (a == b || a == c || b == c) bw.write("Isosceles\n");
-            else bw.write("Scalene\n");
-        }
+        String[] tmp = bf.readLine().split(" ");
+        int a = Integer.parseInt(tmp[0]);
+        int b = Integer.parseInt(tmp[1]);
+        int c = Integer.parseInt(tmp[2]);
+        int max;
+        if (a >= b && a >= c) max = a;
+        else if (b >= a && b >= c) max = b;
+        else max = c;
+        int sum = a + b + c;
+        if (sum - max > max) bw.write(sum + "\n");
+        else bw.write((2 * (sum - max) - 1) + "\n");
         bw.flush();
     }
 }
