3592a6ee949277ab6032f006518914dc823ea6fb
Eichi2514 <picon1317@gmail.com>
2024-09-29 21:32:44 +0900

    백준 : 네 번째 점

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] tmp = bf.readLine().split(" ");
-        int x = Integer.parseInt(tmp[0]);
-        int y = Integer.parseInt(tmp[1]);
-        int w = Integer.parseInt(tmp[2]);
-        int h = Integer.parseInt(tmp[3]);
-        int min = x;
-        if (y < min) min = y;
-        if (w-x < min) min = w-x;
-        if (h-y < min) min = h-y;
-        bw.write(min+"");
+        int[] x = new int[3];
+        int[] y = new int[3];
+        for (int i = 0; i < 3; i++) {
+            String[] tmp = bf.readLine().split(" ");
+            x[i] = Integer.parseInt(tmp[0]);
+            y[i] = Integer.parseInt(tmp[1]);
+        }
+        int x2;
+        int y2;
+        if (x[0] == x[1]) {
+            x2 = x[2];
+        }else if (x[1] == x[2]) {
+            x2 = x[0];
+        }else {
+            x2 = x[1];
+        }
+        if (y[0] == y[1]) {
+            y2 = y[2];
+        }else if (y[1] == y[2]) {
+            y2 = y[0];
+        }else {
+            y2 = y[1];
+        }
+        bw.write(x2+" "+y2);
         bw.flush();
     }
 }
