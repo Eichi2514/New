930a43ebfefc7e895738f80e9e6c22dc5e7851c0
Eichi2514 <picon1317@gmail.com>
2024-09-29 21:46:57 +0900

    백준 : 대지

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,30 +6,35 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int[] x = new int[3];
-        int[] y = new int[3];
-        for (int i = 0; i < 3; i++) {
+        int n = Integer.parseInt(bf.readLine());
+        int[] x = new int[n];
+        int[] y = new int[n];
+        for (int i = 0; i < n; i++) {
             String[] tmp = bf.readLine().split(" ");
             x[i] = Integer.parseInt(tmp[0]);
             y[i] = Integer.parseInt(tmp[1]);
         }
-        int x2;
-        int y2;
-        if (x[0] == x[1]) {
-            x2 = x[2];
-        }else if (x[1] == x[2]) {
-            x2 = x[0];
-        }else {
-            x2 = x[1];
+        int xMin = x[0];
+        int xMax = x[0];
+        int yMin = y[0];
+        int yMax = y[0];
+        for (int i = 0; i < n; i++) {
+            if (x[i] < xMin) {
+                xMin = x[i];
+            }
+            if (x[i] > xMax) {
+                xMax = x[i];
+            }
+            if (y[i] < yMin) {
+                yMin = y[i];
+            }
+            if (y[i] > yMax) {
+                yMax = y[i];
+            }
         }
-        if (y[0] == y[1]) {
-            y2 = y[2];
-        }else if (y[1] == y[2]) {
-            y2 = y[0];
-        }else {
-            y2 = y[1];
-        }
-        bw.write(x2+" "+y2);
+        int asdf = (xMax - xMin) * (yMax - yMin);
+        if (n > 1) bw.write(asdf + "");
+        else bw.write(0 + "");
         bw.flush();
     }
 }
