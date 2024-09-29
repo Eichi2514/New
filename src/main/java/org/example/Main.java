95db21965eb605c90ccc563a8f12096d397c87a2
Eichi2514 <picon1317@gmail.com>
2024-09-29 18:46:21 +0900

    백준 : 소수 찾기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,36 +1,30 @@
 package org.example;
 
 import java.io.*;
-import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        while (true) {
-            String tmp = bf.readLine();
-            if (tmp.equals("-1")) {
-                break;
-            }
-            int n = Integer.parseInt(tmp);
-            int n2 = 0;
-            ArrayList list = new ArrayList();
-            for (int i = 1; i <= n - 1; i++) {
-                if (n % i == 0) {
-                    list.add(i);
-                    n2 += i;
+        int tmp = Integer.parseInt(bf.readLine());
+        String[] tmp2 = bf.readLine().split(" ");
+        int count = 0;
+        for (int i = 0; i < tmp; i++) {
+            int n = Integer.parseInt(tmp2[i]);
+            boolean chack = true;
+            if (n >= 2) {
+                for (int j = 2; j <= Math.sqrt(n); j++) {
+                    if (n % j == 0) {
+                        chack = false;
+                        break;
+                    }
+                }
+                if (chack) {
+                    count++;
                 }
             }
-            if (n2 == n) {
-                bw.write(n+" = 1");
-                for (int i = 1; i < list.size(); i++) {
-                    bw.write(" + "+list.get(i));
-                }
-            }else {
-                bw.write(n+" is NOT perfect.");
-            }
-            bw.newLine();
         }
+        bw.write(count + "");
         bw.flush();
     }
 }
