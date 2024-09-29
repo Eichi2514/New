be2a149f2d05fbd0118444d1df70a82550351ad4
Eichi2514 <picon1317@gmail.com>
2024-09-29 19:10:13 +0900

    백준 : 소인수분해

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,33 +7,14 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int n = Integer.parseInt(bf.readLine());
-        int m = Integer.parseInt(bf.readLine());
-        int min = 0;
-        int max = 0;
-        boolean chack = true;
-        for (int i = n; i <= m; i++) {
-            boolean chack2 = true;
-            if (i >= 2) {
-                for (int j = 2; j * j <= i; j++) {
-                    if (i % j == 0) {
-                        chack2 = false;
-                        break;
-                    }
-                }
-                if (chack2) {
-                    max += i;
-                    if (chack) {
-                        min = i;
-                        chack = false;
-                    }
-                }
+        int a = 2;
+        while (n > 1) {
+            if (n % a == 0) {
+                bw.write(a + "\n");
+                n /= a;
+            } else {
+                a++;
             }
-        }
-        if (max != 0) {
-            bw.write(max + "\n");
-            bw.write(min + "");
-        } else {
-            bw.write("-1");
         }
         bw.flush();
     }
