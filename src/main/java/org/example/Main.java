37e58853b6dc0254885a617ab5142c090b78e559
Eichi2514 <picon1317@gmail.com>
2024-10-03 16:34:35 +0900

    백준 : 알고리즘 수업 - 알고리즘의 수행 시간 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,15 +7,15 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int tmp = Integer.parseInt(bf.readLine());
-        int count = 0;
+        long count = 0;
         int[] A = new int[500000];
         int i = 0;
-        for(int j = 0; j < tmp; j++) {
+        for (int j = 0; j < tmp; j++) {
             count++;
             i = tmp / 2;
         }
-        bw.write(count + "\n");
-        bw.write(A[i]+1 + "");
+        bw.write(count * count + "\n");
+        bw.write(A[i] + 2 + "");
         bw.flush();
     }
 }
