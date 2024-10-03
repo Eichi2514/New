31cb395e84e24a95d88f8bf80344855ab8dddd58
Eichi2514 <picon1317@gmail.com>
2024-10-03 16:56:20 +0900

    백준 : 알고리즘 수업 - 알고리즘의 수행 시간 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,8 +7,8 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         long n = Integer.parseInt(bf.readLine());
-        bw.write((n * (n - 1) / 2) + "\n");
-        bw.write("2");
+        bw.write(n * n * n + "\n");
+        bw.write("3");
         bw.flush();
     }
 }
