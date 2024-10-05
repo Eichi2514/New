991da7f850227f6f661fe0febc21a4d3964e8136
Eichi2514 <picon1317@gmail.com>
2024-10-05 18:03:54 +0900

    백준 : 영화감독 숌

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,57 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        String[] XY = bf.readLine().split(" ");
-        int X = Integer.parseInt(XY[0]);
-        int Y = Integer.parseInt(XY[1]);
-        String[][] map = new String[X][Y];
-        for (int i = 0; i < X; i++) {
-            String[] line = bf.readLine().split("");
-            for (int j = 0; j < Y; j++) {
-                map[i][j] = line[j];
+        int count = Integer.parseInt(bf.readLine());
+        int num = 665;
+        int count2 = 0;
+        while (count != count2) {
+            num++;
+            String tmp = Integer.toString(num);
+            if (tmp.contains("666")) {
+                count2++;
             }
         }
-        int minCount = 64;
-        String[][] pattern1 = {
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"}
-        };
-        String[][] pattern2 = {
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"},
-                {"B", "W", "B", "W", "B", "W", "B", "W"},
-                {"W", "B", "W", "B", "W", "B", "W", "B"}
-        };
-        for (int i = 0; i <= X - 8; i++) {
-            for (int j = 0; j <= Y - 8; j++) {
-                int count1 = 0;
-                int count2 = 0;
-
-                for (int x = 0; x < 8; x++) {
-                    for (int y = 0; y < 8; y++) {
-                        if (!map[i + x][j + y].equals(pattern1[x][y])) {
-                            count1++;
-                        }
-                        if (!map[i + x][j + y].equals(pattern2[x][y])) {
-                            count2++;
-                        }
-                    }
-                }
-                int min = Math.min(count1, count2);
-                minCount = Math.min(minCount, min);
-            }
-        }
-        bw.write(minCount + "");
+        bw.write(num+"");
         bw.flush();
     }
 }
