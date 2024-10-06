b9a96648061335b961be4e6bafd06a1107213dbb
Eichi2514 <picon1317@gmail.com>
2024-10-06 16:38:30 +0900

    코드업 : 1202 등급판정

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,17 @@
 package org.example;
 
-import java.io.*;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        int tmp = Integer.parseInt(bf.readLine());
-        int count = 0;
-        while (tmp >= 0) {
-            if (tmp % 5 == 0) {
-                count += tmp / 5;
-                tmp = 0;
-                break;
-            }
-            tmp -= 3;
-            count++;
-        }
-        if (tmp == 0) {
-            bw.write(count + "");
-        } else {
-            bw.write("-1");
-        }
-        bw.flush();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String ranking = "";
+        if (n >= 90) ranking = "A";
+        else if (n >= 80) ranking = "B";
+        else if (n >= 70) ranking = "C";
+        else if (n >= 60) ranking = "D";
+        else ranking = "F";
+        System.out.println(ranking);
     }
 }
