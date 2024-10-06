d0d2fcc0299f867f87c6bbcb13296e7f15187f1b
Eichi2514 <picon1317@gmail.com>
2024-10-06 16:43:18 +0900

    코드업 : 1203 비만도 측정 0

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,12 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        String ranking = "";
-        if (n >= 90) ranking = "A";
-        else if (n >= 80) ranking = "B";
-        else if (n >= 70) ranking = "C";
-        else if (n >= 60) ranking = "D";
-        else ranking = "F";
-        System.out.println(ranking);
+        String tmp;
+        if (n <= 10) tmp = "정상";
+        else if (n <= 20) tmp = "과체중";
+        else tmp = "비만";
+        System.out.println(tmp);
     }
 }
