ea58c3a3ac3241385bd008dea6c51c1858e4aece
Eichi2514 <picon1317@gmail.com>
2024-10-06 16:53:36 +0900

    코드업 : 1205 최댓값

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        String tmp;
-        if (n % 10 == 1 && 11 != n) tmp = n+"st";
-        else if (n % 10 == 2 && 12 != n) tmp = n+"nd";
-        else if (n % 10 == 3 && 13 != n) tmp = n+"rd";
-        else tmp = n+"th";
-        System.out.println(tmp);
+        double n = sc.nextInt();
+        double m = sc.nextInt();
+        double max = n+m;
+        if (n-m > max) max = n-m;
+        if (m-n > max) max = m-n;
+        if (n*m > max) max = n*m;
+        if (n/m > max) max = n/m;
+        if (m/n > max) max = m/n;
+        if (Math.pow(n,m) > max) max = Math.pow(n,m);
+        if (Math.pow(m,n) > max) max = Math.pow(m,n);
+        System.out.printf("%6f",max);
     }
 }
