70ca04f11ab07127c1ab3180b24bffb3e84292ce
Eichi2514 <picon1317@gmail.com>
2024-10-06 17:11:34 +0900

    코드업 : 1210 칼로리 계산하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int count = 0;
-        for (int i = 0; i < 4; i++) {
-            int tmp = sc.nextInt();
-            if (tmp == 1) count++;
-        }
-        if (count == 1) System.out.println("도");
-        else if (count == 2) System.out.println("개");
-        else if (count == 3) System.out.println("걸");
-        else if (count == 4) System.out.println("윷");
-        else System.out.println("모");
+        int order1 = sc.nextInt();
+        int order2 = sc.nextInt();
+        int[] list = {400,340,170,100,70};
+        int menu1 = list[order1-1];
+        int menu2 = list[order2-1];
+        if(menu1+menu2 > 500) System.out.print("angry");
+        else System.out.println("no angry");
     }
 }
