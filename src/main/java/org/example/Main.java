f0fa4c31f6366ee5f31d9d4c0b0923b0750f4d0a
Eichi2514 <picon1317@gmail.com>
2024-10-07 17:07:36 +0900

    코드업 : 1222 축구의 신 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -9,12 +8,12 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        int[] numbers = {a, b, c};
-        Arrays.sort(numbers);
-        if (numbers[2] >= numbers[0] + numbers[1]) System.out.println("삼각형아님");
-        else if (a == b && b == c) System.out.println("정삼각형");
-        else if (a == b || b == c || a == c) System.out.println("이등변삼각형");
-        else if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) System.out.println("직각삼각형");
-        else System.out.println("삼각형");
+        while (a < 90) {
+            b++;
+            a += 5;
+        }
+        if (b > c) System.out.println("win");
+        else if (b < c) System.out.println("lose");
+        else System.out.println("same");
     }
 }
