ee8592e8bcec825c1eb504c656577610614ee5a7
Eichi2514 <picon1317@gmail.com>
2024-10-07 17:09:28 +0900

    코드업 : 1224 분수 크기 비교

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        while (a < 90) {
-            b++;
-            a += 5;
-        }
-        if (b > c) System.out.println("win");
-        else if (b < c) System.out.println("lose");
-        else System.out.println("same");
+        double a = sc.nextInt();
+        double b = sc.nextInt();
+        double c = sc.nextInt();
+        double d = sc.nextInt();
+        if (a / b  >  c / d) System.out.println(">");
+        else if (a / b ==  c / d) System.out.println("=");
+        else if (a / b  <  c / d) System.out.println("<");
     }
 }
