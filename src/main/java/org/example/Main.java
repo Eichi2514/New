f1bb2cccbd4b8142355f714b67868dfa79068dac
Eichi2514 <picon1317@gmail.com>
2024-10-07 16:37:30 +0900

    코드업 : 1216 컨설팅 회사

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,15 @@
 package org.example;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int year = sc.nextInt();
-        int month = sc.nextInt();
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
-            System.out.println(31);
-        else if(month == 4 || month == 6 || month == 9 || month == 11) System.out.println(30);
-        else if((year%4 == 0 && year%100 != 0)||(year%400 == 0)) System.out.println(29);
-        else System.out.println(28);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        if(b-c > a) System.out.println("advertise");
+        else if(b-c < a) System.out.println("do not advertise");
+        else System.out.println("does not matter");
     }
 }
