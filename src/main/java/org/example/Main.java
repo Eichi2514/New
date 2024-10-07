b9d83bf0a43d1083946311c77670f096ec8729d1
Eichi2514 <picon1317@gmail.com>
2024-10-07 16:47:37 +0900

    코드업 : 1218 삼각형 판단하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +9,12 @@ public class Main {
         int a = sc.nextInt();
         int b = sc.nextInt();
         int c = sc.nextInt();
-        if(b-c > a) System.out.println("advertise");
-        else if(b-c < a) System.out.println("do not advertise");
-        else System.out.println("does not matter");
+        int[] numbers = {a, b, c};
+        Arrays.sort(numbers);
+        if (numbers[2] >= numbers[0] + numbers[1]) System.out.println("삼각형아님");
+        else if (a == b && b == c) System.out.println("정삼각형");
+        else if (a == b || b == c || a == c) System.out.println("이등변삼각형");
+        else if (Math.pow(a, 2) + Math.pow(b, 2) == Math.pow(c, 2)) System.out.println("직각삼각형");
+        else System.out.println("삼각형");
     }
 }
