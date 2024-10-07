30878d7b340e8a213e7fbf768cfdd980b88dc92a
Eichi2514 <picon1317@gmail.com>
2024-10-07 17:35:35 +0900

    코드업 : 1230 터널 통과하기 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,15 +5,21 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double k = sc.nextDouble();
-        double m = sc.nextDouble();
-        double n;
-        if (k < 150) n = k - 100;
-        else if (k >= 160) n = (k - 100) * 0.9;
-        else n = (k - 150) / 2 + 50;
-        double bmi = (m - n) * 100 / n;
-        if (bmi <= 10) System.out.println("정상");
-        else if (bmi > 20) System.out.println("비만");
-        else System.out.println("과체중");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String CRASH = "PASS";
+        String PASS = "";
+        if (a <= 170) {
+            CRASH = "CRASH";
+            PASS = a + "";
+        } else if (b <= 170) {
+            CRASH = "CRASH";
+            PASS = b + "";
+        } else if (c <= 170) {
+            CRASH = "CRASH";
+            PASS = c + "";
+        }
+        System.out.println(CRASH + " " + PASS);
     }
 }
