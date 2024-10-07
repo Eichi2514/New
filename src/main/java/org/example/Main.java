55c52a3f9b9d57bdffb69355452544dc9fc02416
Eichi2514 <picon1317@gmail.com>
2024-10-07 17:18:23 +0900

    코드업 : 1226 이번 주 로또

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        double a = sc.nextInt();
-        double b = sc.nextInt();
-        double c = sc.nextInt();
-        double d = sc.nextInt();
-        if (a / b  >  c / d) System.out.println(">");
-        else if (a / b ==  c / d) System.out.println("=");
-        else if (a / b  <  c / d) System.out.println("<");
+        int[] lotto = new int[6];
+        int[] my = new int[6];
+        for (int i = 0; i < 6; i++) {
+            int tmp = sc.nextInt();
+            lotto[i] = tmp;
+        }
+        int bo = sc.nextInt();
+        for (int i = 0; i < 6; i++) {
+            int tmp = sc.nextInt();
+            my[i] = tmp;
+        }
+        int count = 0;
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                if (lotto[i] == my[j]) {
+                    count++;
+                }
+            }
+        }
+        if (count != 5) {
+            if (count <= 2) System.out.println(0);
+            else if (count == 3) System.out.println(5);
+            else if (count == 4) System.out.println(4);
+            else if (count == 6) System.out.println(1);
+        } else if (count == 5) {
+            for (int i = 0; i < 6; i++) {
+                if (bo == my[i]) {
+                    count++;
+                }
+            }
+            if (count == 5) System.out.println(3);
+            else if (count == 6) System.out.println(2);
+        }
     }
 }
