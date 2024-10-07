de5a91cb5466dee307798d1109a3f5ec1a5b0c35
Eichi2514 <picon1317@gmail.com>
2024-10-07 16:34:39 +0900

    코드업 : 1214 이 달은 며칠까지 있을까?

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
-        String tmp = "no";
-        int[] numbers = {a, b, c};
-        Arrays.sort(numbers);
-        if (numbers[2] < numbers[0] + numbers[1]) tmp = "yes";
-        System.out.println(tmp);
+        int year = sc.nextInt();
+        int month = sc.nextInt();
+        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12)
+            System.out.println(31);
+        else if(month == 4 || month == 6 || month == 9 || month == 11) System.out.println(30);
+        else if((year%4 == 0 && year%100 != 0)||(year%400 == 0)) System.out.println(29);
+        else System.out.println(28);
     }
 }
