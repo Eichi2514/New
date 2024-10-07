c138659191302d8aeb8bd58c6e3363e55adda7c5
Eichi2514 <picon1317@gmail.com>
2024-10-07 16:29:03 +0900

    코드업 : 1212 삼각형의 성립 조건

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,18 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int order1 = sc.nextInt();
-        int order2 = sc.nextInt();
-        int[] list = {400,340,170,100,70};
-        int menu1 = list[order1-1];
-        int menu2 = list[order2-1];
-        if(menu1+menu2 > 500) System.out.print("angry");
-        else System.out.println("no angry");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        String tmp = "no";
+        int[] numbers = {a, b, c};
+        Arrays.sort(numbers);
+        if (numbers[2] < numbers[0] + numbers[1]) tmp = "yes";
+        System.out.println(tmp);
     }
 }
