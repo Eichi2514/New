ebd5914ddfda719d1f21a5b0b3f6b16ad5ef7657
Eichi2514 <picon1317@gmail.com>
2024-10-08 19:59:00 +0900

    코드업 : 1256 별 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        for (double i = 0; i < a; i++) {
-            System.out.print("*");
+        int b = sc.nextInt();
+        for (int i = a; i <= b; i++) {
+            if (i % 2 != 0) System.out.print(i + " ");
         }
     }
 }
