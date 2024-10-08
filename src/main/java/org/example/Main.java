50766603dcf2205975ad48bc0d2adf073a141e60
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:20:53 +0900

    코드업) 1275 : k 제곱 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,13 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        boolean flag = true;
-        for (int i = 2; i < n; i++) {
-            if (n % i == 0) {
-                flag = false;
-            }
+        int k = sc.nextInt();
+        int sum = 1;
+        for (int i = 0; i < k; i++) {
+            sum *= n;
         }
-        if (flag) System.out.println("prime");
-        else System.out.println("not prime");
+        System.out.println(sum);
     }
 }
