05e609eba4cf5dc66df49f8e06aaa2684956971b
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:22:53 +0900

    코드업) 1276 : 팩토리얼 계산

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int k = sc.nextInt();
         int sum = 1;
-        for (int i = 0; i < k; i++) {
-            sum *= n;
+        for (int i = 2; i <= n; i++) {
+            sum *= i;
         }
         System.out.println(sum);
     }
