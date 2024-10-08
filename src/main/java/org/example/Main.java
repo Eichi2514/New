faa4be6595f024e9a35627dfdc4ab5a21ba227bb
Eichi2514 <picon1317@gmail.com>
2024-10-08 20:00:24 +0900

    코드업 : 1256 별 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,9 +6,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
-        int b = sc.nextInt();
-        for (int i = a; i <= b; i++) {
-            if (i % 2 != 0) System.out.print(i + " ");
+        int b = 0;
+        for (int i = 1; i <= a; i++) {
+            b += i;
         }
+        System.out.println(b);
     }
 }
