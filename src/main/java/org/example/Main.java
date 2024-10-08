169ff51c9f5d18ba216022cdfeab0d9759a9fc52
Eichi2514 <picon1317@gmail.com>
2024-10-08 19:42:49 +0900

    코드업 : 1253 a 부터 b까지 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,16 @@
 package org.example;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int[] tmps = {a, b};
+        Arrays.sort(tmps);
+        for (int i = tmps[0]; i <= tmps[1]; i++) {
             System.out.print(i + " ");
         }
     }
