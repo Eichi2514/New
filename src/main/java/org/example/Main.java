3758586d36c3aa4e74f439f134a92f010e1e5d95
Eichi2514 <picon1317@gmail.com>
2024-10-08 19:56:16 +0900

    코드업 : 1255 두 실수 사이 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,10 +5,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        char a = sc.next().charAt(0);
-        char b = sc.next().charAt(0);
-        for (char c = a; c <= b; c++) {
-            System.out.print(c + " ");
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        for (double i = a; i <= b; i += 0.01) {
+            System.out.printf("%.02f ", i);
         }
     }
 }
