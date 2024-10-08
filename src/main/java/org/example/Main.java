f701b14524a590cfa2344b43790fcbc7a5e02449
Eichi2514 <picon1317@gmail.com>
2024-10-08 20:27:49 +0900

    코드업) 1267 : n개의 수 중 5의 배수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,7 +9,7 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            sum += a;
+            if (a % 5 == 0) sum += a;
         }
         System.out.println(sum);
     }
