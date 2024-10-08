9352fe32e0ec063a794f2ea72f2aad6599fb6089
Eichi2514 <picon1317@gmail.com>
2024-10-08 20:32:51 +0900

    코드업) 1269 : 수열의 값 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
         int n = sc.nextInt();
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            int a = sc.nextInt();
-            if (a % 2 != 0) sum++;
+        if(n == 1) System.out.println(a);
+        else {
+            for(int i = 2; i <= n; i++) {
+                a = a*b+c;
+            }
+            System.out.println(a);
         }
-        System.out.println(sum);
     }
 }
