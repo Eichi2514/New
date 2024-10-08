29346761d3ea22f556479a0419fd72315e6d6d15
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:14:02 +0900

    코드업) 1273 : 약수 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,24 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        long sum = 0;
-        long tmp = 0;
-        for (int j = 0; j < 2; j++) {
-            long a = 1;
-            long b = 10;
-            int n = sc.nextInt();
-            for (int i = 1; i <= n; i++) {
-                if (i % 2 != 0) {
-                    tmp = a;
-                    a++;
-                }
-                if (i % 2 == 0) {
-                    tmp = b;
-                    b += 10;
-                }
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                System.out.print(i + " ");
             }
-            sum += tmp;
         }
-        System.out.println(sum);
     }
 }
