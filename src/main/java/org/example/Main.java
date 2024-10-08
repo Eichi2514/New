057f7f194bf431186577fce42738bbae99dcc6b5
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:31:26 +0900

    코드업) 1278 : 자릿수 계산

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int[] nums = new int[n];
-        for (int i = 0; i < n; i++) {
-            nums[i] = sc.nextInt();
+        int count = 0;
+        while (n >= 1) {
+            count++;
+            n /= 10;
         }
-        System.out.print(nums[0]+" "+nums[n/2]+" "+nums[n-1]);
+        System.out.println(count);
     }
 }
