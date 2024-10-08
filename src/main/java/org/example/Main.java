acc5cb70ebdc707610020fadf12b8d2d040361fe
Eichi2514 <picon1317@gmail.com>
2024-10-08 20:34:54 +0900

    코드업) 1270 : 1의 개수는? 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,16 +5,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = sc.nextInt();
         int n = sc.nextInt();
-        if(n == 1) System.out.println(a);
-        else {
-            for(int i = 2; i <= n; i++) {
-                a = a*b+c;
-            }
-            System.out.println(a);
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % 10 == 1) count++;
         }
+        System.out.println(count);
     }
 }
