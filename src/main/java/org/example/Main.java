42abca9674ef44a703fe0e893d05048093cf403b
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:11:30 +0900

    코드업) 1272 : 기부

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,12 +5,24 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int max = 0;
-        for (int i = 1; i <= n; i++) {
-            int a = sc.nextInt();
-            if (max < a) max = a;
+        long sum = 0;
+        long tmp = 0;
+        for (int j = 0; j < 2; j++) {
+            long a = 1;
+            long b = 10;
+            int n = sc.nextInt();
+            for (int i = 1; i <= n; i++) {
+                if (i % 2 != 0) {
+                    tmp = a;
+                    a++;
+                }
+                if (i % 2 == 0) {
+                    tmp = b;
+                    b += 10;
+                }
+            }
+            sum += tmp;
         }
-        System.out.println(max);
+        System.out.println(sum);
     }
 }
