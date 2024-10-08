29632739c59bdc473d4d515e87b24a0904e16582
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:33:36 +0900

    코드업) 1279 : 홀수는 더하고 짝수는 빼고 1

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 0;
-        while (n >= 1) {
-            count++;
-            n /= 10;
+        int m = sc.nextInt();
+        int sum = 0;
+        for (int i = n; i <= m; i++) {
+            if (i % 2 != 0) sum += i;
+            else sum -= i;
         }
-        System.out.println(count);
+        System.out.println(sum);
     }
 }
