b219f06cdfacdeafb6315a9f51a4545a8d99fd53
Eichi2514 <picon1317@gmail.com>
2024-10-08 21:18:32 +0900

    코드업) 1274 : 소수 판별

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,10 +6,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
+        boolean flag = true;
+        for (int i = 2; i < n; i++) {
             if (n % i == 0) {
-                System.out.print(i + " ");
+                flag = false;
             }
         }
+        if (flag) System.out.println("prime");
+        else System.out.println("not prime");
     }
 }
