1fbf674682dd8f999be234b34f8ae28ac7c09017
Eichi2514 <picon1317@gmail.com>
2024-10-08 19:40:15 +0900

    코드업 : 1252 1 부터 n 까지 출력하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        for(int i = 1; i <= 100; i++){
-            System.out.print(i+" ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
         }
     }
 }
