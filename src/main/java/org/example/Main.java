d389daeab4714c17d819171caad64f793b7117ca
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:55:56 +0900

    프로그래머스) 없는 숫자 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,11 +1,16 @@
 package org.example;
 
 class Solution {
-    public int solution(int[] absolutes, boolean[] signs) {
+    public int solution(int[] numbers) {
         int answer = 0;
-        for (int i = 0; i < absolutes.length; i++) {
-            if (signs[i]) answer += absolutes[i];
-            else answer -= absolutes[i];
+        for (int j = 0; j < 10; j++) {
+            boolean chack = true;
+            for (int i = 0; i < numbers.length; i++) {
+                if (numbers[i] == j) {
+                    chack = false;
+                }
+            }
+            if (chack) answer += j;
         }
         return answer;
     }
@@ -13,10 +18,10 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-        int[] asd = {4, 7, 12};
         // String[] qwe = {"Jane", "Kim"};
-        boolean[] zxc = {true, false, true};
-        System.out.println(new Solution().solution(asd, zxc));
+        int[] asd = {1,2,3,4,6,7,8,0};
+        // boolean[] zxc = {true, false, true};
+        System.out.println(new Solution().solution(asd));
     }
 }
 
