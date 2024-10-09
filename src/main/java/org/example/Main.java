ec3ec872e555b54389e9d323896ff67e1b1b8296
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:02:44 +0900

    프로그래머스) 자릿수 더하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,15 +3,17 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution(2));
+        System.out.println(new Solution().solution(123));
     }
 }
 
 class Solution {
-    public String solution(int num) {
-        String answer = "";
-        if (num%2 != 0) answer = "Odd";
-        else answer = "Even";
+    public int solution(int n) {
+        int answer = 0;
+        while (n > 0) {
+            answer += n % 10;
+            n /= 10;
+        }
         return answer;
     }
 }
