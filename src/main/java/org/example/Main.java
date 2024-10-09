da312da91961aa1365b5367fc18469ce204d8384
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:14:25 +0900

    프로그래머스) 자연수 뒤집어 배열로 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,21 @@
 package org.example;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution("pPoooyY"));
+        System.out.println(new Solution().solution(12345));
     }
 }
 
 class Solution {
-    boolean solution(String s) {
-        boolean answer = true;
-        int Pcount = 0;
-        int Ycount = 0;
-        String[] tmps = s.split("");
-        for (int i = 0; i < tmps.length; i++) {
-            if (tmps[i].equals("P") || tmps[i].equals("p")) Pcount++;
-            else if (tmps[i].equals("Y") || tmps[i].equals("y")) Ycount++;
+    public ArrayList<Long> solution(long n) {
+        ArrayList<Long> answer = new ArrayList<>();
+        while (n > 0) {
+            answer.add(n % 10);
+            n /= 10;
         }
-        if (Pcount != Ycount) answer = false;
         return answer;
     }
 }
