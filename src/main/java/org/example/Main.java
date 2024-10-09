d26e2763deb0bc51052e938e3c70c0e8e85e65c3
Eichi2514 <picon1317@gmail.com>
2024-10-09 13:46:28 +0900

    프로그래머스) 문자열을 정수로 바꾸기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,19 +4,13 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int sum = 0;
-        for (int i = n; i <= m; i++) {
-            if (i % 2 != 0) {
-                System.out.print("+" + i);
-                sum += i;
-            } else {
-                System.out.print("-" + i);
-                sum -= i;
-            }
-        }
-        System.out.println("=" + sum);
+        System.out.println(Solution.solution("123"));
+    }
+}
+
+class Solution {
+    public static int solution(String s) {
+        int answer = Integer.parseInt(s);
+        return answer;
     }
 }
