f2e84b508178bc5269c3f35690e1efdc5cad9341
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:04:07 +0900

    프로그래머스) 약수의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution(123));
+        System.out.println(new Solution().solution(12));
     }
 }
 
 class Solution {
     public int solution(int n) {
         int answer = 0;
-        while (n > 0) {
-            answer += n % 10;
-            n /= 10;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) answer += i;
         }
         return answer;
     }
