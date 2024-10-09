f0eaf7a326440429d06499ce49d4ee9fbfe738cf
Eichi2514 <picon1317@gmail.com>
2024-10-09 13:57:56 +0900

    프로그래머스) x만큼 간격이 있는 n개의 숫자

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,20 +1,18 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4};
-        System.out.println(Solution.solution(arr));
+        // int[] arr = {1,2,3,4};
+        System.out.println(new Solution().solution(2, 5));
     }
 }
 
 class Solution {
-    public static double solution(int[] arr) {
-        double answer = 0;
-        for (int i = 0; i < arr.length; i++) {
-            answer += arr[i];
+    public long[] solution(int x, int n) {
+        long[] answer = new long[n];
+        for (int i = 0; i < n; i++) {
+            answer[i] = (long) x * (i + 1);
         }
-        return answer/arr.length;
+        return answer;
     }
 }
