06a91b7a7efb25fdfedb0c9167b5ad28b406d5eb
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:08:37 +0900

    프로그래머스) 문자열 내 p와 y의 개수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,21 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution(12));
+        System.out.println(new Solution().solution("pPoooyY"));
     }
 }
 
 class Solution {
-    public int solution(int n) {
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 0) answer += i;
+    boolean solution(String s) {
+        boolean answer = true;
+        int Pcount = 0;
+        int Ycount = 0;
+        String[] tmps = s.split("");
+        for (int i = 0; i < tmps.length; i++) {
+            if (tmps[i].equals("P") || tmps[i].equals("p")) Pcount++;
+            else if (tmps[i].equals("Y") || tmps[i].equals("y")) Ycount++;
         }
+        if (Pcount != Ycount) answer = false;
         return answer;
     }
 }
