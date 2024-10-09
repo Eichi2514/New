16cbbdda4c0bcf59d78f05c0896ee79024e4ffd5
Eichi2514 <picon1317@gmail.com>
2024-10-09 14:22:23 +0900

    정수 내림차순으로 배치하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,29 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.Collections;
+
+class Solution {
+    public long solution(long n) {
+        ArrayList<Long> tmps = new ArrayList<>();
+        while (n > 0) {
+            tmps.add(n % 10);
+            n /= 10;
+        }
+        Collections.sort(tmps, Collections.reverseOrder());
+        String tmp = "";
+        for (int i = 0; i < tmps.size(); i++) {
+            tmp += tmps.get(i);
+        }
+        long answer = Long.parseLong(tmp.toString());
+        return answer;
+    }
+}
 
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution(12345));
+        System.out.println(new Solution().solution(118372));
     }
 }
 
-class Solution {
-    public ArrayList<Long> solution(long n) {
-        ArrayList<Long> answer = new ArrayList<>();
-        while (n > 0) {
-            answer.add(n % 10);
-            n /= 10;
-        }
-        return answer;
-    }
-}
