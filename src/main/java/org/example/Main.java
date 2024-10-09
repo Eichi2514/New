011b8e8e26f3151b9796ed9989d673e01c59c260
Eichi2514 <picon1317@gmail.com>
2024-10-09 13:51:32 +0900

    프로그래머스) 평균 구하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,17 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Solution.solution(10));
+        int[] arr = {1,2,3,4};
+        System.out.println(Solution.solution(arr));
     }
 }
 
 class Solution {
-    public static int solution(int n) {
-        for (int i = 1; i <= n; i++) {
-            if (n % i == 1) {
-                return i;
-            }
+    public static double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
         }
-        return 0;
+        return answer/arr.length;
     }
 }
