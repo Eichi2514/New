b7009d6f1ba1e5333da232bb69b2018020abecde
Eichi2514 <picon1317@gmail.com>
2024-10-09 13:59:49 +0900

    프로그래머스) 짝수와 홀수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,16 +3,15 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         // int[] arr = {1,2,3,4};
-        System.out.println(new Solution().solution(2, 5));
+        System.out.println(new Solution().solution(2));
     }
 }
 
 class Solution {
-    public long[] solution(int x, int n) {
-        long[] answer = new long[n];
-        for (int i = 0; i < n; i++) {
-            answer[i] = (long) x * (i + 1);
-        }
+    public String solution(int num) {
+        String answer = "";
+        if (num%2 != 0) answer = "Odd";
+        else answer = "Even";
         return answer;
     }
 }
