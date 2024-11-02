dd4dc2faebe85486c884c18a3983fcb1137b15fa
Eichi2514 <picon1317@gmail.com>
2024-11-02 21:58:54 +0900

    프로그래머스) 약수의 개수와 덧셈

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,15 +1,28 @@
 package org.example;
 
 class Solution {
-    public String solution(int n) {
-        String answer = "";
-        for (int i = 1; i <= n; i++){
-            if(i%2 == 1) answer += "수";
-            else answer += "박";
+    public int solution(int left, int right) {
+        int answer = 0;
+        for (int i = left; i <= right; i++) {
+            int count = 0;
+            for (int j = 1; j * j <= i; j++) {
+                if (i % j == 0) {
+                    count++;
+                    if (j != i / j) {
+                        count++;
+                    }
+                }
+            }
+            if (count % 2 == 0) {
+                answer += i;
+            } else {
+                answer -= i;
+            }
         }
         return answer;
     }
 }
+
 
 public class Main {
     public static void main(String[] args) {
@@ -17,7 +30,7 @@ public class Main {
         // int[] asd = {1, 2, 3, 4};
         // int[] asd2 = {-3, -1, 0, 2};
         // boolean[] zxc = {true, false, true};
-        System.out.println(new Solution().solution(3));
+        System.out.println(new Solution().solution(13, 17));
     }
 }
 
