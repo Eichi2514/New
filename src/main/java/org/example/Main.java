ccef3ea5f071ebeac7c6dfb733f7be8ee3e9c42f
Eichi2514 <picon1317@gmail.com>
2024-11-02 22:10:57 +0900

    프로그래머스) 문자열 내림차순으로 배치하기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,15 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 class Solution {
-    public int solution(int left, int right) {
-        int answer = 0;
-        for (int i = left; i <= right; i++) {
-            int count = 0;
-            for (int j = 1; j * j <= i; j++) {
-                if (i % j == 0) {
-                    count++;
-                    if (j != i / j) {
-                        count++;
-                    }
-                }
-            }
-            if (count % 2 == 0) {
-                answer += i;
-            } else {
-                answer -= i;
-            }
+    public String solution(String s) {
+        String[] arr = s.split("");
+        Arrays.sort(arr, Collections.reverseOrder());
+        String answer = "";
+        for (String str : arr) {
+            answer += str;
         }
         return answer;
     }
@@ -30,7 +22,7 @@ public class Main {
         // int[] asd = {1, 2, 3, 4};
         // int[] asd2 = {-3, -1, 0, 2};
         // boolean[] zxc = {true, false, true};
-        System.out.println(new Solution().solution(13, 17));
+        System.out.println(new Solution().solution("Zbcdefg"));
     }
 }
 
