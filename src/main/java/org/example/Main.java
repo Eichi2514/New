c879efe62b3ad617e6fb66bff7421885210c685d
Eichi2514 <picon1317@gmail.com>
2024-11-03 15:26:47 +0900

    프로그래머스) 직사각형 별찍기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,13 +1,18 @@
 package org.example;
 
+import java.util.Scanner;
+
 class Solution {
-    public int[][] solution(int[][] arr1, int[][] arr2) {
-        for(int i = 0; i < arr1.length; i++) {
-            for(int j = 0; j < arr1[i].length; j++) {
-                arr1[i][j] = arr1[i][j] + arr2[i][j];
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (int i = 0; i < b; i++) {
+            for (int j = 0; j < a; j++) {
+                System.out.print("*");
             }
+            System.out.println();
         }
-        return arr1;
     }
 }
 
@@ -18,9 +23,8 @@ public class Main {
         // int[] asd = {1, 2, 3, 4};
         // int[] asd2 = {-3, -1, 0, 2};
         // boolean[] zxc = {true, false, true};
-        int[][] asd3 = {{1,2}, {2,3}};
-        int[][] asd4 = {{3,4}, {5,6}};
-        System.out.println(new Solution().solution(asd3, asd4));
+        // int[][] asd3 = {{1,2}, {2,3}};
+        // int[][] asd4 = {{3,4}, {5,6}};
     }
 }
 
