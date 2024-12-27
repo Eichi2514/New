aa1af6fad03841293236d4435cf6f4e4d493f053
Eichi2514 <picon1317@gmail.com>
2024-12-27 17:21:36 +0900

    프로그래머스) [PCCE 기출문제] 10번 / 공원

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,81 +1,53 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.Arrays;
 
 class Solution {
-    public int[] solution(String[] park, String[] routes) {
-        int w = 0;
-        int h = 0;
-        for(int i = 0; i < park.length; i++){
-            for(int j = 0; j < park[i].length(); j++){
-                if(park[i].charAt(j) == 'S'){
-                    h = i;
-                    w = j;
-                    break;
+    public int solution(int[] mats, String[][] park) {
+        int answer = -1;
+
+        Arrays.sort(mats);
+
+        for (int i = mats.length - 1; i >= 0; i--) {
+            for (int j = 0; j <= park.length - mats[i]; j++) {
+                for (int k = 0; k <= park[j].length - mats[i]; k++) {
+                    boolean tmp = true;
+                    for (int l = j; l < j + mats[i]; l++) {
+                        for (int m = k; m < k + mats[i]; m++) {
+                            if (!park[l][m].equals("-1")) {
+                                tmp = false;
+                                break;
+                            }
+                        }
+                        if (!tmp) break;
+                    }
+                    if (tmp) return mats[i];
                 }
             }
         }
-
-        for (int i = 0; i < routes.length; i++) {
-            boolean tmp = true;
-            if (routes[i].charAt(0) == 'E') {
-                for (int j = w; j < Integer.parseInt(String.valueOf(routes[i].charAt(2))); j++) {
-                    if (park[h].charAt(j) == 'X') {
-                        tmp = false;
-                    }
-                }
-                if (tmp && w + Integer.parseInt(String.valueOf(routes[i].charAt(2))) < park[0].length()) {
-                    w += Integer.parseInt(String.valueOf(routes[i].charAt(2)));
-                }
-            } else if (routes[i].charAt(0) == 'W') {
-                for (int j = w; j >= Integer.parseInt(String.valueOf(routes[i].charAt(2))); j--) {
-                    if (park[h].charAt(j) == 'X') {
-                        tmp = false;
-                    }
-                }
-                if (tmp && w - Integer.parseInt(String.valueOf(routes[i].charAt(2))) >= 0) {
-                    w -= Integer.parseInt(String.valueOf(routes[i].charAt(2)));
-                }
-            } else if (routes[i].charAt(0) == 'S') {
-                for (int j = h; j < Integer.parseInt(String.valueOf(routes[i].charAt(2))); j++) {
-                    if (park[j].charAt(w) == 'X') {
-                        tmp = false;
-                    }
-                }
-                if (tmp && h + Integer.parseInt(String.valueOf(routes[i].charAt(2))) < park.length) {
-                    h += Integer.parseInt(String.valueOf(routes[i].charAt(2)));
-                }
-            } else if (routes[i].charAt(0) == 'N') {
-                for (int j = h; j >= Integer.parseInt(String.valueOf(routes[i].charAt(2))); j--) {
-                    if (park[j].charAt(w) == 'X') {
-                        tmp = false;
-                    }
-                }
-                if (tmp && h - Integer.parseInt(String.valueOf(routes[i].charAt(2))) >= 0) {
-                    h -= Integer.parseInt(String.valueOf(routes[i].charAt(2)));
-                }
-            }
-        }
-
-        int[] answer = {h, w};
         return answer;
     }
 }
 
 public class Main {
     public static void main(String[] args) {
-        String[] qwe = {"SOO","OXX","OOO"};
-        String[] qwe2 = {"E 2","S 2","W 1"};
-        // int[] asd = {1, 2, 3, 4};
+        // String[] qwe = {"SOO","OXX","OOO"};
+        // String[] qwe2 = {"E 2","S 2","W 1"};
+        String[][] qwe =
+                {
+                        {"1", "-1"},
+                        {"1", "-1"}
+                };
+        int[] asd = {1};
         // int[] asd2 = {-3, -1, 0, 2};
         // boolean[] zxc = {true, false, true};
         // int[][] asd3 = {{1,2}, {2,3}};
         // int[][] asd4 = {{3,4}, {5,6}};
-        int[] asd = new Solution().solution(qwe,qwe2);
-        for (int i = 0; i < asd.length; i++) {
-            System.out.println(asd[i]);
-        }
-//        System.out.println(new Solution().solution("z-+.^."));
+        // int[] asd = new Solution().solution(qwe,qwe2);
+//         for (int i = 0; i < asd.length; i++) {
+//             System.out.println(asd[i]);
+//         }
+          System.out.println(new Solution().solution(asd, qwe));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
