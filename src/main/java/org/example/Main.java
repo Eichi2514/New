884bd77cb2ca2532c1845986581bbf96a897b2d9
Eichi2514 <picon1317@gmail.com>
2024-12-29 20:44:14 +0900

    프로그래머스) [PCCP 기출문제] 1번 / 붕대 감기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,49 +1,33 @@
 package org.example;
 
-import java.util.*;
-
 class Solution {
-    public int[] solution(String[] id_list, String[] report, int k) {
-        HashMap<String, Set<String>> reportMap = new HashMap<>();
-        HashMap<String, Integer> reportedCountMap = new HashMap<>();
+    public int solution(int[] bandage, int health, int[][] attacks) {
+        int answer = health;
+        int count = 0;
+        int time = 0;
+        int index = 0;
 
-        for (String id : id_list) {
-            reportMap.put(id, new HashSet<>());
-        }
-
-        for (String r : report) {
-            String[] parts = r.split(" ");
-            String reporter = parts[0];
-            String reported = parts[1];
-            reportMap.get(reporter).add(reported);
-        }
-
-        for (Set<String> reportedSet : reportMap.values()) {
-            for (String reported : reportedSet) {
-                reportedCountMap.put(reported, reportedCountMap.getOrDefault(reported, 0) + 1);
-            }
-        }
-
-        HashSet<String> suspendedUsers = new HashSet<>();
-        for (String user : reportedCountMap.keySet()) {
-            if (reportedCountMap.get(user) >= k) {
-                suspendedUsers.add(user);
-            }
-        }
-
-        int[] answer = new int[id_list.length];
-        for (int i = 0; i < id_list.length; i++) {
-            String user = id_list[i];
-            int mailCount = 0;
-
-            for (String reported : reportMap.get(user)) {
-                if (suspendedUsers.contains(reported)) {
-                    mailCount++;
+        while (answer >= 0 && index < attacks.length) {
+            count++;
+            time++;
+            if (time == attacks[index][0]) {
+                answer -= attacks[index][1];
+                index++;
+                count = 0;
+            } else {
+                if (answer + bandage[1] < health) answer += bandage[1];
+                else answer = health;
+                if (count == bandage[0]) {
+                    count = 0;
+                    if (answer + bandage[2] < health) {
+                        answer += bandage[2];
+                    } else {
+                        answer = health;
+                    }
                 }
             }
-            answer[i] = mailCount;
         }
-
+        if (answer <= 0) return -1;
         return answer;
     }
 }
@@ -51,23 +35,23 @@ class Solution {
 
 public class Main {
     public static void main(String[] args) {
-        String[] qwe = {"muzi", "frodo", "apeach", "neo"};
-        String[] qwe2 = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        // String[] qwe = {"muzi", "frodo", "apeach", "neo"};
+        // String[] qwe2 = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
         // String[][] qwe =
         //         {
         //                 {"1", "-1"},
         //                 {"1", "-1"}
         //         };
-        // int[] asd = {1};
+        int[] asd = {3, 10, 1};
         // int[] asd2 = {-3, -1, 0, 2};
         // boolean[] zxc = {true, false, true};
-        // int[][] asd3 = {{1,2}, {2,3}};
+        int[][] asd3 = {{1, 5}, {3, 5}};
         // int[][] asd4 = {{3,4}, {5,6}};
-        int[] asd = new Solution().solution(qwe, qwe2, 2);
-        for (int i = 0; i < asd.length; i++) {
-            System.out.println(asd[i]);
-        }
-//        System.out.println(new Solution().solution(asd, qwe));
+//        int[] asd = new Solution().solution(qwe, qwe2, 2);
+//        for (int i = 0; i < asd.length; i++) {
+//            System.out.println(asd[i]);
+//        }
+        System.out.println(new Solution().solution(asd, 100, asd3));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
