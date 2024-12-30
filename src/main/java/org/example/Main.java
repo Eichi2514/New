80a2eac6c1a060fb41008e8419476580bdc3391d
Eichi2514 <picon1317@gmail.com>
2024-12-30 16:33:58 +0900

    프로그래머스) 가장 많이 받은 선물

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,75 +1,81 @@
 package org.example;
 
+import java.util.HashMap;
+
 class Solution {
-    public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        String[] tmps = {video_len, pos, op_start, op_end};
-        int[][] times = new int[4][2];
+    public int solution(String[] friends, String[] gifts) {
+        HashMap<String, Integer> friendMap = new HashMap<>();
+        HashMap<String, Integer> giftMap = new HashMap<>();
 
-        for (int i = 0; i < 4; i++) {
-            String[] cmd = tmps[i].split(":");
-            for (int j = 0; j < 2; j++) {
-                times[i][j] = Integer.parseInt(cmd[j]);
+        for (int i = 0; i < friends.length; i++) {
+            friendMap.put(friends[i], 0);
+        }
+
+        for (int i = 0; i < gifts.length; i++) {
+            int giftsCount = 1;
+
+            if (giftMap.get(gifts[i]) != null) giftsCount = giftMap.get(gifts[i]) + 1;
+            giftMap.put(gifts[i], giftsCount);
+
+            String[] tmps = gifts[i].split(" ");
+            friendMap.put(tmps[0], friendMap.get(tmps[0]) + 1);
+            friendMap.put(tmps[1], friendMap.get(tmps[1]) - 1);
+        }
+
+        HashMap<String, Integer> answerMap = new HashMap<>();
+
+        for (int i = 0; i < friends.length; i++) {
+            for (int j = 0; j < friends.length; j++) {
+                String StrIndex1 = friends[i] + " " + friends[j];
+                String StrIndex2 = friends[j] + " " + friends[i];
+
+                if (i != j) {
+                    if (giftMap.get(StrIndex1) != null && giftMap.get(StrIndex2) != null) {
+                        if (giftMap.get(StrIndex1) > giftMap.get(StrIndex2)) {
+                            int count = 0;
+                            if (answerMap.get(friends[i]) != null) count = answerMap.get(friends[i]);
+                            answerMap.put(friends[i], count + 1);
+                        } else if (giftMap.get(StrIndex1) == giftMap.get(StrIndex2)) {
+                            if (friendMap.get(friends[i]) > friendMap.get(friends[j])) {
+                                int count = 0;
+                                if (answerMap.get(friends[i]) != null) count = answerMap.get(friends[i]);
+                                answerMap.put(friends[i], count + 1);
+                            }
+                        }
+                    } else if (giftMap.get(StrIndex1) != null) {
+                        int count = 0;
+                        if (answerMap.get(friends[i]) != null) count = answerMap.get(friends[i]);
+                        answerMap.put(friends[i], count + 1);
+                    } else if (giftMap.get(StrIndex1) == null && giftMap.get(StrIndex2) == null) {
+                        if (friendMap.get(friends[i]) > friendMap.get(friends[j])) {
+                            int count = 0;
+                            if (answerMap.get(friends[i]) != null) count = answerMap.get(friends[i]);
+                            answerMap.put(friends[i], count + 1);
+                        }
+                    }
+                }
             }
         }
 
-        times[1] = op_reset(times[1], times[2], times[3]);
+        if (answerMap.size() == 0) return 0;
 
-        for (int i = 0; i < commands.length; i++) {
-            if (commands[i].equals("next")) {
-                times[1][1] += 10;
-                if (times[1][1] >= 60) {
-                    times[1][0]++;
-                    times[1][1] -= 60;
-                }
-                if(times[1][0] > times[0][0]) {
-                    times[1][0] = times[0][0];
-                    times[1][1] = times[0][1];
-                }else if (times[1][0] == times[0][0] && times[1][1] > times[0][1]) {
-                    times[1][1] = times[0][1];
-                }
-            } else if (commands[i].equals("prev")) {
-                times[1][1] -= 10;
-                if (times[1][1] < 0) {
-                    times[1][0]--;
-                    times[1][1] += 60;
-                }
-                if(times[1][0] < 0) {
-                    times[1][0] = 0;
-                    times[1][1] = 0;
-                }else if (times[1][0] == 0 && times[1][1] < 0) {
-                    times[1][1] = 0;
-                }
+        int answer = 0;
+
+        for (int i = 0; i < friends.length; i++) {
+            if (answerMap.get(friends[i]) != null) {
+                if (answer < answerMap.get(friends[i])) answer = answerMap.get(friends[i]);
             }
-            times[1] = op_reset(times[1], times[2], times[3]);
         }
 
-        String mm = Integer.toString(times[1][0]);
-        String ss = Integer.toString(times[1][1]);
-        if (mm.length() < 2) mm = "0" + mm;
-        if (ss.length() < 2) ss = "0" + ss;
-        return mm + ":" + ss;
-    }
-
-    private int[] op_reset(int[] time, int[] start, int[] end) {
-        if (start[0] < time[0] && time[0] < end[0]) {
-            return new int[]{end[0], end[1]};
-        } else if (start[0] != end[0] && start[0] == time[0] && start[1] <= time[1]) {
-            return new int[]{end[0], end[1]};
-        } else if (start[0] != end[0] && end[0] == time[0] && time[1] <= end[1]) {
-            return new int[]{end[0], end[1]};
-        } else if (start[0] == time[0] && end[0] == time[0] && start[1] <= time[1] && time[1] <= end[1]) {
-            return new int[]{end[0], end[1]};
-        }
-        return time;
+        return answer;
     }
 }
 
 
-
 public class Main {
     public static void main(String[] args) {
-        String[] qwe = {"prev", "next"};
-        // String[] qwe2 = {"muzi frodo", "apeach frodo", "frodo neo", "muzi neo", "apeach muzi"};
+        String[] qwe = {"muzi", "ryan", "frodo", "neo"};
+        String[] qwe2 = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
         // String[][] qwe =
         //         {
         //                 {"1", "-1"},
@@ -84,7 +90,7 @@ public class Main {
 //        for (int i = 0; i < asd.length; i++) {
 //            System.out.println(asd[i]);
 //        }
-        System.out.println(new Solution().solution("10:00", "00:03", "00:00", "00:05", qwe));
+        System.out.println(new Solution().solution(qwe, qwe2));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
