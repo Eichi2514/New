98ba4b55f4271e28b781ccf1c257103e0b3552b2
Eichi2514 <picon1317@gmail.com>
2024-12-31 16:50:39 +0900

    백준) 문자열 집합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,31 +1,39 @@
 package org.example;
 
 import java.io.*;
-import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(bf.readLine());
-        String[] tmps1 = bf.readLine().split(" ");
+        String[] tmps = bf.readLine().split(" ");
 
-        int m = Integer.parseInt(bf.readLine());
-        String[] tmps2 = bf.readLine().split(" ");
+        int n = Integer.parseInt(tmps[0]);
+        int m = Integer.parseInt(tmps[1]);
 
-        HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < m; i++) {
-            map.put(tmps2[i], 0);
-        }
+        String[] tmps1 = new String[n];
+        String[] tmps2 = new String[m];
+
         for (int i = 0; i < n; i++) {
-            map.put(tmps1[i], 1);
+            String tmp = bf.readLine();
+            tmps1[i] = tmp;
         }
 
         for (int i = 0; i < m; i++) {
-            bw.write(map.get(tmps2[i]) + " ");
+            String tmp = bf.readLine();
+            tmps2[i] = tmp;
         }
 
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmps1[i].equals(tmps2[j])) count++;
+            }
+        }
+
+        bw.write(count + " ");
         bw.flush();
     }
 }
