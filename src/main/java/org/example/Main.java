d9f8e3bb6f5d0abaaa4d2d5111e0724343747c94
Eichi2514 <picon1317@gmail.com>
2025-01-03 01:32:14 +0900

    백준) 최소공배수2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,25 +7,22 @@ public class Main {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int n = Integer.parseInt(bf.readLine());
+        String[] s = bf.readLine().split(" ");
+        long m = Long.parseLong(s[0]);
+        long l = Long.parseLong(s[1]);
 
-        for (int i = 0; i < n; i++) {
-            String[] s = bf.readLine().split(" ");
-            int m = Integer.parseInt(s[0]);
-            int l = Integer.parseInt(s[1]);
-
-            int a = m;
-            int b = l;
-            while (b != 0) {
-                int tmp = b;
-                b = a % b;
-                a = tmp;
-            }
-
-            int min = (m * l) / a;
-
-            bw.write(min + "\n");
+        long a = m;
+        long b = l;
+        while (b != 0) {
+            long tmp = b;
+            b = a % b;
+            a = tmp;
         }
+
+        long min = (m * l) / a;
+
+        bw.write(min + "\n");
+
         bw.flush();
     }
 }
