b26d6b6b5238eb7f37e40853ae70fb1d5c7b79ab
Eichi2514 <picon1317@gmail.com>
2025-01-14 16:50:14 +0900

    프로그래머스) JadenCase 문자열 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,21 +1,24 @@
 package org.example;
 
-import java.util.*;
+class Solution {
+    public static String solution(String s) {
+        String answer = "";
+        String tmp = " " + s;
 
-class Solution{
-    public int solution(int []A, int []B) {
-        int answer = 0;
-
-        Arrays.sort(A);
-        Arrays.sort(B);
-
-        for(int i = 1; i <= A.length; i++){
-            answer += A[i-1] * B[B.length-i];
+        for (int i = 1; i <= s.length(); i++) {
+            if (tmp.charAt(i-1) == ' '){
+                String t = tmp.charAt(i) + "";
+                answer += t.toUpperCase();
+            } else {
+                String t = tmp.charAt(i) + "";
+                answer += t.toLowerCase();
+            }
         }
 
         return answer;
     }
 }
+
 
 public class Main {
     public static void main(String[] args) {
@@ -26,8 +29,8 @@ public class Main {
         //                 {"1", "-1"},
         //                 {"1", "-1"}
         //         };
-        int[] asd = {1, 4, 2};
-        int[] asd2 = {5, 4, 4};
+        //int[] asd = {1, 4, 2};
+        //int[] asd2 = {5, 4, 4};
         // boolean[] zxc = {true, false, true};
         // int[][] asd3 = {{1, 5}, {3, 5}};
         // int[][] asd4 = {{3,4}, {5,6}};
@@ -35,7 +38,7 @@ public class Main {
 //        for (int i = 0; i < asd.length; i++) {
 //            System.out.println(asd[i]);
 //        }
-        System.out.println(new Solution().solution(asd, asd2));
+        System.out.println(Solution.solution("  for the what 1what  "));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
