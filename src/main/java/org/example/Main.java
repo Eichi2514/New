fc0e4e2c8c277c60e1500b98e6cd9f36a39bfb4e
Eichi2514 <picon1317@gmail.com>
2025-01-16 16:56:25 +0900

    프로그래머스) 귤 고르기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,38 +3,34 @@ package org.example;
 import java.util.*;
 
 class Solution {
-    public int[] solution(int n, String[] words) {
+    public int solution(int k, int[] tangerine) {
+        Map<Integer, Integer> map = new HashMap<>();
 
-        ArrayList<String> arr = new ArrayList<>();
-        arr.add(words[0]);
-        for (int i = 1; i < words.length; i++){
-            int tmp = 1;
-            int count = 1;
-            if (words[i-1].charAt(words[i-1].length()-1) != words[i].charAt(0)){
-                for(int k = 1; k <= i; k++){
-                    if (tmp < n) tmp++;
-                    else {
-                        count++;
-                        tmp = 1;
-                    }
-                }
-                return new int[]{tmp, count};
-            }
-            for(int j = 0; j < arr.size(); j++){
-                if (words[i].equals(arr.get(j))){
-                    for(int k = 1; k <= i; k++){
-                        if (tmp < n) tmp++;
-                        else {
-                            count++;
-                            tmp = 1;
-                        }
-                    }
-                    return new int[]{tmp, count};
-                }
-            }
-            arr.add(words[i]);
+        for (int size : tangerine) {
+            map.put(size, map.getOrDefault(size, 0) + 1);
         }
-        return new int[] {0, 0};
+
+        Integer[] arr = new Integer[map.size()];
+        int index = 0;
+
+        for (int value : map.values()) {
+            arr[index++] = value;
+        }
+
+        Arrays.sort(arr, (a, b) -> b - a);
+
+        int count = 0;
+        int answer = 0;
+
+        for (int a : arr) {
+            count += a;
+            answer++;
+            if (count >= k) {
+                break;
+            }
+        }
+
+        return answer;
     }
 }
 
@@ -53,10 +49,12 @@ public class Main {
         // boolean[] zxc = {true, false, true};
         // int[][] asd3 = {{1, 5}, {3, 5}};
         // int[][] asd4 = {{3,4}, {5,6}};
+/*
         int[] asd = new Solution().solution(3, qwe);
         for (int i = 0; i < asd.length; i++) {
             System.out.println(asd[i]);
         }
+ */
 //        System.out.println(new Solution().solution(5000));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
