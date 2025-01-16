8e442fab9fb6cf4329e3b78f547c9ba04b8ae370
Eichi2514 <picon1317@gmail.com>
2025-01-16 15:47:55 +0900

    프로그래머스) N개의 최소공배수

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,19 +1,23 @@
 package org.example;
 
+import java.util.*;
+
 class Solution {
-    public int solution(int n) {
-        int tmp = 1234567;
+    public int solution(int[] arr) {
+        Arrays.sort(arr);
 
-        int[] answer = new int[n + 1];
+        int answer = arr[0];
 
-        answer[1] = 1;
-        if (n > 1) answer[2] = 2;
-
-        for (int i = 3; i <= n; i++) {
-            answer[i] = (answer[i - 1] + answer[i - 2]) % tmp;
+        while(true){
+            int count = 0;
+            for(int i = 0; i < arr.length; i++){
+                if (answer % arr[i] == 0) count++;
+            }
+            if(count == arr.length) break;
+            else answer += arr[0];
         }
 
-        return answer[n];
+        return answer;
     }
 }
 
@@ -27,7 +31,7 @@ public class Main {
         //                 {"1", "-1"},
         //                 {"1", "-1"}
         //         };
-        // int[] asd = {100, 50, 100, 50, 40};
+        int[] asd = {2,6,8,14};
         //int[] asd2 = {5, 4, 4};
         // boolean[] zxc = {true, false, true};
         // int[][] asd3 = {{1, 5}, {3, 5}};
@@ -36,7 +40,7 @@ public class Main {
 //        for (int i = 0; i < asd.length; i++) {
 //            System.out.println(asd[i]);
 //        }
-        System.out.println(new Solution().solution(150));
+        System.out.println(new Solution().solution(asd));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
