2dda308244db5d298cb650e3eeadf8cdaf6314f8
Eichi2514 <picon1317@gmail.com>
2025-01-16 16:07:13 +0900

    프로그래머스) 점프와 순간 이동

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,23 +1,18 @@
 package org.example;
 
-import java.util.*;
-
 class Solution {
-    public int solution(int[] arr) {
-        Arrays.sort(arr);
+    public int solution(int n) {
+        int ans = 0;
 
-        int answer = arr[0];
-
-        while(true){
-            int count = 0;
-            for(int i = 0; i < arr.length; i++){
-                if (answer % arr[i] == 0) count++;
+        while(n > 0){
+            if(n % 2 == 1){
+                n--;
+                ans++;
             }
-            if(count == arr.length) break;
-            else answer += arr[0];
+            n /= 2;
         }
 
-        return answer;
+        return ans;
     }
 }
 
@@ -31,7 +26,7 @@ public class Main {
         //                 {"1", "-1"},
         //                 {"1", "-1"}
         //         };
-        int[] asd = {2,6,8,14};
+        //int[] asd = {2,6,8,14};
         //int[] asd2 = {5, 4, 4};
         // boolean[] zxc = {true, false, true};
         // int[][] asd3 = {{1, 5}, {3, 5}};
@@ -40,7 +35,7 @@ public class Main {
 //        for (int i = 0; i < asd.length; i++) {
 //            System.out.println(asd[i]);
 //        }
-        System.out.println(new Solution().solution(asd));
+        System.out.println(new Solution().solution(5000));
 //        System.out.println(new Solution().solution("=.="));
 //        System.out.println(new Solution().solution("123_.def"));
 //        System.out.println(new Solution().solution("abcdefghijklmn.p"));
