6a469e62e11091072d855d65e441851e71aa3678
Eichi2514 <picon1317@gmail.com>
2025-05-01 14:10:33 +0900

    코드업) 1360 : 숫자 피라미드 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,55 +1,16 @@
 package org.example;
 
-class Solution{
-    public int solution(int n, int a, int b) {
-        int answer = 0;
-
-        for(int i = 1; i <= n; i++){
-            if (a % 2 == 0){
-                if (a-1 == b) return i;
-
-                a /= 2;
-
-                if (b % 2 == 0) b /= 2;
-                else b = (b + 1) / 2;
-            } else{
-                if (a+1 == b) return i;
-
-                a = (a + 1) / 2;
-
-                if (b % 2 == 0) b /= 2;
-                else b = (b + 1) / 2;
-            }
-        }
-
-        return answer;
-    }
-}
-
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        // String[] qwe = {"abc", "cbd", "ddd", "ddd", "dbc", "cbd"};
-        // String[] qwe2 = {"muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"};
-        // String[][] qwe =
-        //         {
-        //                 {"1", "-1"},
-        //                 {"1", "-1"}
-        //         };
-        //int[] asd = {2,6,8,14};
-        //int[] asd2 = {5, 4, 4};
-        // boolean[] zxc = {true, false, true};
-        // int[][] asd3 = {{1, 5}, {3, 5}};
-        // int[][] asd4 = {{3,4}, {5,6}};
-/*
-        int[] asd = new Solution().solution(3, qwe);
-        for (int i = 0; i < asd.length; i++) {
-            System.out.println(asd[i]);
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
         }
- */
-//        System.out.println(new Solution().solution(5000));
-//        System.out.println(new Solution().solution("=.="));
-//        System.out.println(new Solution().solution("123_.def"));
-//        System.out.println(new Solution().solution("abcdefghijklmn.p"));
     }
 }
