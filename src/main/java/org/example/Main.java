e7cfe2057671d8d8bf8f8ee46fb756dfdbf958ba
Eichi2514 <picon1317@gmail.com>
2025-05-02 12:47:21 +0900

    코드업) 1361 : 별 계단 만들기

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = n; i >= 1; i--) {
+        for (int i = 1; i <= n; i++) {
+            System.out.println("**");
             for (int j = 1; j <= i; j++) {
-                System.out.print(i + " ");
+                System.out.print(" ");
             }
-            System.out.println();
         }
     }
 }
