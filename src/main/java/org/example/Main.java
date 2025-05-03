a578a9a18d5f7978894db7281a8519ec74899d2d
Eichi2514 <picon1317@gmail.com>
2025-05-03 23:54:09 +0900

    코드업) 1362 : 숫자 피라미드 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,11 +6,18 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for (int i = 1; i <= n; i++) {
-            System.out.println("**");
+        int count = 0;
+        for (int i = n; i >= 1; i--) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(" ");
+                count++;
             }
+        }
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(count + " ");
+                count--;
+            }
+            System.out.println();
         }
     }
 }
