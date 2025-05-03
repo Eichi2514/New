06629e26e94c6989e5c3f6051d1d898d950f13b6
Eichi2514 <picon1317@gmail.com>
2025-05-04 05:00:02 +0900

    코드업) 1365 : 사각형 출력하기 3

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,16 +6,14 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int count = 0;
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                count++;
-            }
-        }
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(count + " ");
-                count--;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0 || i == j || i == n - j - 1 || i == n - 1 || j == n - 1) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
             }
             System.out.println();
         }
