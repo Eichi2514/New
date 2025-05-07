7a38ec071f1d439fef1c581cd16ffb56e6787dfb
Eichi2514 <picon1317@gmail.com>
2025-05-07 18:14:07 +0900

    코드업) 1368 : 평행사변형 출력하기 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,14 +5,26 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        String[] tmps = sc.nextLine().split(" ");
+        int n = Integer.parseInt(tmps[0]);
+        int k = Integer.parseInt(tmps[1]);
+        boolean d = tmps[2].equals("L");
 
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*");
+            if (d) {
+                for (int j = 1; j <= i - 1; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= k; j++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int j = 1; j <= n - i; j++) {
+                    System.out.print(" ");
+                }
+                for (int j = 1; j <= k; j++) {
+                    System.out.print("*");
+                }
             }
             System.out.println();
         }
