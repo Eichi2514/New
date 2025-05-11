11463d2e5ae08b35fd5bd6f41cf4e8affde30233
Eichi2514 <picon1317@gmail.com>
2025-05-11 21:19:49 +0900

    코드업) 1378 : 수열의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,31 +3,18 @@ package org.example;
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int s = 2;
-        int m = n - 1;
+        int Sn = 0;
 
-        for (int i = 1; i <= n * 2; i++) {
-            for (int j = 1; j <= m; j++) {
-                System.out.print(" ");
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                Sn += j;
             }
-            for (int j = 1; j <= s; j++) {
-                if (j == 1 || j == s) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            if (i < n) {
-                m--;
-                s += 2;
-            } else if (i > n) {
-                m++;
-                s -= 2;
-            }
-            System.out.println();
         }
+
+        System.out.println(Sn);
     }
 }
