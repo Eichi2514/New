22abfdf5a8be59220382d584c57fce3ab2efdb74
Eichi2514 <picon1317@gmail.com>
2025-05-12 00:28:14 +0900

    코드업) 1382 : GuguClass

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,18 +1,12 @@
 package org.example;
 
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        for (int i = 1; i <= 6; i++) {
-            for (int j = 1; j <= 6; j++) {
-                if (i + j == n) {
-                    System.out.println(i + " " + j);
-                }
+        for (int i = 1; i <= 9; i++) {
+            for (int j = 2; j <= 5; j++) {
+                System.out.printf("%d x %d = %2d", j, i, i*j);
+                if (j != 5) System.out.print("\t");
+                else System.out.println();
             }
         }
     }
