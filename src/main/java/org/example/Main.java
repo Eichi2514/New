e6339fc1f4fca793876f9e18f9cc08862abc940b
Eichi2514 <picon1317@gmail.com>
2025-05-12 00:09:25 +0900

    코드업) 1380 : 두 주사위의 합

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,14 +7,13 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int Sn = 0;
 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                Sn += j;
+        for (int i = 1; i <= 6; i++) {
+            for (int j = 1; j <= 6; j++) {
+                if (i + j == n) {
+                    System.out.println(i + " " + j);
+                }
             }
         }
-
-        System.out.println(Sn);
     }
 }
