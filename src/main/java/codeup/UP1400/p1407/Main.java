9153bb0fc0a4318a1d71cda9f5f2963457f87503
Eichi2514 <picon1317@gmail.com>
2025-05-18 13:02:41 +0900

    코드업) 1407 : 문자열 출력하기 1

---
+++ src/main/java/codeup/UP1400/p1407/Main.java
@@ -0,0 +1,10 @@
+package codeup.UP1400.p1407;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLine().replaceAll(" ",""));
+    }
+}
