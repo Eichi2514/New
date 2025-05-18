b7a39bafb1e8b1c25e22c35bed6e24b3f9f54533
Eichi2514 <picon1317@gmail.com>
2025-05-18 13:00:44 +0900

    코드업) 1406 : love

---
+++ src/main/java/codeup/UP1400/p1406/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1400.p1406;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String love = sc.nextLine();
+        if (love.equals("love")) System.out.println("I love you.");
+    }
+}
