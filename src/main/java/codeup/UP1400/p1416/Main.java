cb15ba61788bcb68ca1f2b5df06d55537bf55f44
Eichi2514 <picon1317@gmail.com>
2025-05-21 19:28:00 +0900

    코드업) 1416 : 2진수 변환

---
+++ src/main/java/codeup/UP1400/p1416/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1400.p1416;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String n = Integer.toString(sc.nextInt(), 2);
+        System.out.println(n);
+    }
+}
