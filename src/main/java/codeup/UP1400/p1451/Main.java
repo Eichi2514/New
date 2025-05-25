a02453a3765cbdecaf620393e49d93343e1872b4
Eichi2514 <picon1317@gmail.com>
2025-05-25 14:19:14 +0900

    코드업) 1451 : 데이터 정렬 (small)

---
+++ src/main/java/codeup/UP1400/p1451/Main.java
@@ -0,0 +1,24 @@
+package codeup.UP1400.p1451;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n;
+
+        n = sc.nextInt();
+
+        int[] ns = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            ns[i] = sc.nextInt();
+        }
+
+        Arrays.sort(ns);
+
+        for (int N : ns) {
+            System.out.println(N);
+        }
+    }
+}
