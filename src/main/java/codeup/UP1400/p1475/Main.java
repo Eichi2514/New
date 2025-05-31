4a48bf801a2b60e9114a314e38374063117d15d4
Eichi2514 <picon1317@gmail.com>
2025-05-31 17:38:21 +0900

    코드업) 1475 : [기초-배열연습] 2차원 배열 지그재그 채우기 2-8

---
+++ src/main/java/codeup/UP1400/p1475/Main.java
@@ -0,0 +1,32 @@
+package codeup.UP1400.p1475;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][m];
+        int tmp = 1;
+
+        for (int i = m - 1; i >= 0; i--) {
+            if (m % 2 == 0 && i % 2 != 0 || m % 2 != 0 && i % 2 == 0) {
+                for (int j = 0; j < n; j++) {
+                    a[j][i] = tmp++;
+                }
+            } else {
+                for (int j = n - 1; j >= 0; j--) {
+                    a[j][i] = tmp++;
+                }
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
