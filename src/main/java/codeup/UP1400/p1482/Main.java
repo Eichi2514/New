8a4f22e38458d0a841b5f8814b41a5785f2fdc03
Eichi2514 <picon1317@gmail.com>
2025-06-02 14:05:43 +0900

    코드업) 1482 : [기초-배열연습] 2차원 배열 빗금 채우기 3-7

---
+++ src/main/java/codeup/UP1400/p1482/Main.java
@@ -0,0 +1,29 @@
+package codeup.UP1400.p1482;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[][] a = new int[n][m];
+        int tmp = 1;
+
+        for (int i = 0; i < n + m - 1; i++) {
+            for (int j = 0; j <= i; j++) {
+                int k = i - j;
+                if (j < n && k < m) {
+                    a[j][k] = tmp++;
+                }
+            }
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
