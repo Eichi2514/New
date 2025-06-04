8dfca9b94ef2903d7a44111a683f527f7e5a029a
Eichi2514 <picon1317@gmail.com>
2025-06-04 13:55:51 +0900

    코드업) 1486 : [기초-배열연습] 2차원 배열 달팽이 채우기 4-3

---
+++ src/main/java/codeup/UP1400/p1486/Main.java
@@ -0,0 +1,51 @@
+package codeup.UP1400.p1486;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+        int[][] a = new int[n][m];
+
+        int tmp = 1;
+        int top = 0;
+        int bottom = n - 1;
+        int left = 0;
+        int right = m - 1;
+
+        while (top <= bottom && left <= right) {
+            for (int i = top; i <= bottom; i++) {
+                a[i][right] = tmp++;
+            }
+            right--;
+
+            for (int i = right; i >= left; i--) {
+                a[bottom][i] = tmp++;
+            }
+            bottom--;
+
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    a[i][left] = tmp++;
+                }
+                left++;
+            }
+
+            if (top <= bottom) {
+                for (int i = left; i <= right; i++) {
+                    a[top][i] = tmp++;
+                }
+                top++;
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
