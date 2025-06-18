4f5f5049f72dbdbe23327cb4556398940e2c2a56
Eichi2514 <picon1317@gmail.com>
2025-06-18 13:49:20 +0900

    코드업) 1528 : [기초-함수작성] 함수로 *문자 출력하기

---
+++ src/main/java/codeup/UP1500/p1528/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1528;
+
+public class Main {
+    private static void f() {
+        System.out.println("*");
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
