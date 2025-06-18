265cae5279cf34ba949fdf838e07fb4d025a652f
Eichi2514 <picon1317@gmail.com>
2025-06-18 13:46:40 +0900

    코드업) 1527 : [기초-함수작성] 함수로 123 값 출력하기

---
+++ src/main/java/codeup/UP1500/p1527/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1527;
+
+public class Main {
+    private static void f() {
+        System.out.println(123);
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
