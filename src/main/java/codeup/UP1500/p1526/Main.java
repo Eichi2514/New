a64f4bae82628f284486591ddbcbf634094fd507
Eichi2514 <picon1317@gmail.com>
2025-06-18 13:45:44 +0900

    코드업) 1526 : [기초-함수작성] 함수로 hello 문자열 출력하기

---
+++ src/main/java/codeup/UP1500/p1526/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1526;
+
+public class Main {
+    private static void f() {
+        System.out.println("hello");
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
