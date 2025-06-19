f83fcbdd4aca8ece5dc6eb9bcbe55bac6dafec26
Eichi2514 <picon1317@gmail.com>
2025-06-19 17:33:57 +0900

    코드업) 1530 : [기초-함수작성] 함수로 문자 A 리턴하기

---
+++ src/main/java/codeup/UP1500/p1530/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1530;
+
+public class Main {
+    private static char f() {
+        return 'A';
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f());
+    }
+}
