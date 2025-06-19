ac92e0585e6f286b496aaab0b39a5a856a71b61f
Eichi2514 <picon1317@gmail.com>
2025-06-19 17:34:47 +0900

    코드업) 1531 : [기초-함수작성] 함수로 정수(int) 1 리턴하기

---
+++ src/main/java/codeup/UP1500/p1531/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1531;
+
+public class Main {
+    private static int f() {
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f());
+    }
+}
