5e03ff864c308738167d17ca385bf9b81502db45
Eichi2514 <picon1317@gmail.com>
2025-06-20 16:42:28 +0900

    코드업) 1532 : [기초-함수작성] 함수로 정수(long long int) -2147483649 리턴하기

---
+++ src/main/java/codeup/UP1500/p1532/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1532;
+
+public class Main {
+    private static long f() {
+        return -2147483649L;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(f());
+    }
+}
