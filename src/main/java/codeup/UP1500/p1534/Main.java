c9d06c903bc16dde3ef23a73f53d986b189325fb
Eichi2514 <picon1317@gmail.com>
2025-06-20 16:46:44 +0900

    코드업) 1534 : [기초-함수작성] 함수로 실수(double) 3.1415926535897 리턴하기

---
+++ src/main/java/codeup/UP1500/p1534/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1534;
+
+public class Main {
+    private static double f() {
+        return 3.1415926535897;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%.13f", f());
+    }
+}
