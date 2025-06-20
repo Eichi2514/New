0d3e3a1690a1f9a073f9e85f23c2919823756241
Eichi2514 <picon1317@gmail.com>
2025-06-20 16:44:46 +0900

    코드업) 1533 : [기초-함수작성] 함수로 실수(float) 3.14 리턴하기

---
+++ src/main/java/codeup/UP1500/p1533/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1533;
+
+public class Main {
+    private static float f() {
+        return 3.14f;
+    }
+
+    public static void main(String[] args) {
+        System.out.printf("%f", f());
+    }
+}
