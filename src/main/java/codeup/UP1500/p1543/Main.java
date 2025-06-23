b0ad71728b6f579b23f1d4c0cf347cb6563ebdd3
Eichi2514 <picon1317@gmail.com>
2025-06-23 22:02:40 +0900

    코드업) 1543 : [기초-함수작성] 함수로 love 출력하기

---
+++ src/main/java/codeup/UP1500/p1543/Main.java
@@ -0,0 +1,17 @@
+package codeup.UP1500.p1543;
+
+import java.util.Scanner;
+
+public class Main {
+    private static void f(int n) {
+        for (int i = 0; i < n; i++) {
+            System.out.println("love");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        f(n);
+    }
+}
