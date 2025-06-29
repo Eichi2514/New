1d69ab385f437c1c5f6c125469f8b1defab4a107
Eichi2514 <picon1317@gmail.com>
2025-06-29 14:34:13 +0900

    코드업) 1561 : [기초-함수작성] 함수로 두 정수의 큰 값 리턴하기

---
+++ src/main/java/codeup/UP1500/p1561/Main.java
@@ -0,0 +1,17 @@
+package codeup.UP1500.p1561;
+
+import java.util.Scanner;
+
+public class Main {
+    private static long f(long n, long m) {
+        return Math.max(n, m);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.printf("%d", f(n, m));
+    }
+}
