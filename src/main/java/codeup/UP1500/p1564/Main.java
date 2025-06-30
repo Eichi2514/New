bd7cdb2ff2e3536f27498fa76c734bf1804f116e
Eichi2514 <picon1317@gmail.com>
2025-06-30 19:49:26 +0900

    코드업) 1564 : [기초-함수작성] 함수로 최대공약수 리턴하기

---
+++ src/main/java/codeup/UP1500/p1564/Main.java
@@ -0,0 +1,23 @@
+package codeup.UP1500.p1564;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+
+        System.out.printf("%d", gcd(a, b));
+    }
+}
