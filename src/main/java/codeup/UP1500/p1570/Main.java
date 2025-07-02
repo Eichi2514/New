4c2120d0bcdcb07c41bb7b0b8f524a0042270531
Eichi2514 <picon1317@gmail.com>
2025-07-02 16:27:49 +0900

    코드업) 1570 : [기초-함수작성] 함수로 Lower Bound 위치 리턴하기

---
+++ src/main/java/codeup/UP1500/p1570/Main.java
@@ -0,0 +1,26 @@
+package codeup.UP1500.p1570;
+
+import java.util.Scanner;
+
+public class Main {
+    private static long findi(int p, int[] d) {
+        for (int i = 1; i < d.length; i++) {
+            if (d[i] >= p) return i;
+        }
+        return d.length;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] d = new int[n + 1];
+
+        for (int i = 0; i < n; i++) {
+            d[i + 1] = sc.nextInt();
+        }
+
+        int a = sc.nextInt();
+
+        System.out.printf("%d", findi(a, d));
+    }
+}
