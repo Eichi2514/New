9e477e510e016a6c359a704f61b315b3466d8696
Eichi2514 <picon1317@gmail.com>
2025-07-03 14:54:08 +0900

    코드업) 1576 : (함수 작성) void형 함수

---
+++ src/main/java/codeup/UP1500/p1576/Main.java
@@ -0,0 +1,11 @@
+package codeup.UP1500.p1576;
+
+public class Main {
+    private static void f() {
+        System.out.println("Hello?");
+    }
+
+    public static void main(String[] args) {
+        f();
+    }
+}
