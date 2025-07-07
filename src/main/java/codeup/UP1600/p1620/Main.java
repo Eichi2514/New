335965106f0dc1a663d4d1c3498051cc680db6d9
Eichi2514 <picon1317@gmail.com>
2025-07-07 12:44:53 +0900

    코드업) 1620 : 자릿수의 합

---
+++ src/main/java/codeup/UP1600/p1620/Main.java
@@ -0,0 +1,30 @@
+package codeup.UP1600.p1620;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static int S(int n) {
+        int sum = 0;
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if (sum < 10) {
+            return sum;
+        } else {
+            return S(sum);
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int sum = S(n);
+
+        System.out.println(sum);
+    }
+}
