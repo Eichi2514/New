df5a998da651587a7c9879311a15b9ffef986460
Eichi2514 <picon1317@gmail.com>
2025-07-16 21:28:20 +0900

    코드업) 1711 : 충돌 여부 확인

---
+++ src/main/java/codeup/UP1700/p1710/Main.java
@@ -0,0 +1,19 @@
+package codeup.UP1700.p1710;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x1 = sc.nextInt();
+        int y1 = sc.nextInt();
+        int x2 = sc.nextInt();
+        int y2 = sc.nextInt();
+
+        int xa = sc.nextInt();
+        int ya = sc.nextInt();
+
+        if (x1 <= xa && xa <= x2 && y1 <= ya && ya <= y2) System.out.println("충돌");
+        else System.out.println("비충돌");
+    }
+}
