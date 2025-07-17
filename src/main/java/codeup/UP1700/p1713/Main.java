077036db00a0f3540dafac3ebc2616e4bfbd908e
Eichi2514 <picon1317@gmail.com>
2025-07-17 18:33:25 +0900

    코드업) 1713 : 두 배수의 합과 차

---
+++ src/main/java/codeup/UP1700/p1713/Main.java
@@ -0,0 +1,23 @@
+package codeup.UP1700.p1713;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n1 = sc.nextInt();
+        int n2 = sc.nextInt();
+        int sum = 0;
+
+        for (int i = n1; i <= n2; i++) {
+            if (i % 3 == 0) {
+                sum += i;
+            }
+            if (i % 4 == 0) {
+                sum -= i;
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
