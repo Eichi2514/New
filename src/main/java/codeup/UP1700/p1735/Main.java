4dcb4ee98de4c7317e95ce4fced32ebaa2077bdb
Eichi2514 <picon1317@gmail.com>
2025-07-21 16:41:16 +0900

    코드업) 1735 : hello

---
+++ src/main/java/codeup/UP1700/p1735/Main.java
@@ -0,0 +1,15 @@
+package codeup.UP1700.p1735;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("hello");
+        }
+    }
+}
