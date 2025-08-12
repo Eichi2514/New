b0760bba56b550e0d42c5c182c9da87956ef977a
Eichi2514 <picon1317@gmail.com>
2025-08-12 22:50:38 +0900

    프로그래머스) 행렬의 곱셈

---
+++ src/main/java/programmers/LV2/p12949/Main.java
@@ -0,0 +1,23 @@
+package programmers.LV2.p12949;
+
+class Solution {
+    public int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
+
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr1[0].length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Solution().solution(new int[][]{{1, 4}, {3, 2}, {4, 1}}, new int[][]{{3, 3}, {3, 3}}));
+        System.out.println(new Solution().solution(new int[][]{{2, 3, 2}, {4, 2, 4}, {3, 1, 4}}, new int[][]{{5, 4, 3}, {2, 4, 1}, {3, 1, 1}}));
+    }
+}
