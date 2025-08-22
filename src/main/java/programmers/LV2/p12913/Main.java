9efd7a58ade4a8d985bfc744d96804cbc1c14359
Eichi2514 <picon1317@gmail.com>
2025-08-23 01:05:08 +0900

    프로그래머스) 땅따먹기

---
+++ src/main/java/programmers/LV2/p12913/Main.java
@@ -0,0 +1,32 @@
+package programmers.LV2.p12913;
+
+class Solution {
+    int solution(int[][] land) {
+        for (int i = 1; i < land.length; i++) {
+            for (int j = 0; j < 4; j++) {
+                int max = 0;
+                for (int k = 0; k < 4; k++) {
+                    if (j != k) {
+                        max = Math.max(max, land[i - 1][k]);
+                    }
+                }
+                land[i][j] += max;
+            }
+        }
+
+        int answer = 0;
+        for (int j = 0; j < 4; j++) {
+            answer = Math.max(answer, land[land.length - 1][j]);
+        }
+
+        return answer;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(new int[][]{{1, 2, 3, 5}, {5, 6, 7, 8}, {4, 3, 2, 1}}));
+    }
+}
