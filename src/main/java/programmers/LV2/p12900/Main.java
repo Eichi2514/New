7ac345f1b289b7ef317c8a2489a72c80e6b4019c
Eichi2514 <picon1317@gmail.com>
2025-08-26 17:37:49 +0900

    프로그래머스) 2 x n 타일링

---
+++ src/main/java/programmers/LV2/p12900/Main.java
@@ -0,0 +1,28 @@
+package programmers.LV2.p12900;
+
+class Solution {
+    public int solution(int n) {
+        int MOD = 1_000_000_007;
+
+        if (n == 1) return 1;
+        if (n == 2) return 2;
+
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD;
+        }
+
+        return dp[n];
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        System.out.println(sol.solution(4));
+    }
+}
